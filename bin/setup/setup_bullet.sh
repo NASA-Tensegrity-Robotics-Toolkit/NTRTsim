366d8c686cb821aaa9508db9b08fe66676c7347b
brian <brt6@case.edu>
2014-07-29 11:03:39 -0400

    fixing comment location in setup

--- bin/setup/setup_bullet.sh
+++ bin/setup/setup_bullet.sh
@@ -188,10 +188,9 @@ function build_bullet() {
     -DCMAKE_EXE_LINKER_FLAGS="-fPIC" \
     -DCMAKE_MODULE_LINKER_FLAGS="-fPIC" \
     -DCMAKE_SHARED_LINKER_FLAGS="-fPIC" \
-    -DUSE_DOUBLE_PRECISION=ON \ #If you turn this on, turn it on in inc.CMakeBullet.txt as well for the NTRT build
-
+    -DUSE_DOUBLE_PRECISION=ON \ 
         -DCMAKE_INSTALL_NAME_DIR="$BULLET_INSTALL_PREFIX" || { echo "- ERROR: CMake for Bullet Physics failed."; exit 1; }
-
+	#If you turn this on, turn it on in inc.CMakeBullet.txt as well for the NTRT build
     # Additional bullet options: 
         # -DFRAMEWORK=ON
         # -DBUILD_DEMOS=ON
