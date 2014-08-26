fd7a09993389ee45ccaa7994d10a7366f9a1e515
Perry Bhandal <perry.bhandal@gmail.com>
2014-08-26 15:45:23 -0400

    Added skeleton for 3_prism_serialize

---
+++ src/examples/3_prism_serialize/PrismModel.cpp
@@ -0,0 +1,251 @@
+/*
+ * Copyright © 2012, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ * 
+ * The NASA Tensegrity Robotics Toolkit (NTRT) v1 platform is licensed
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+*/
+
+/**
+ * @file PrismModel.cpp
+ * @brief Contains the definition of the members of the class PrismModel.
+ * $Id$
+ */
+
+// This module
+#include "PrismModel.h"
+// This library
+#include "controllers/PretensionController.h"
+#include "core/tgLinearString.h"
+#include "core/tgRod.h"
+#include "tgcreator/tgBuildSpec.h"
+#include "tgcreator/tgLinearStringInfo.h"
+#include "tgcreator/tgRodInfo.h"
+#include "tgcreator/tgStructure.h"
+#include "tgcreator/tgStructureInfo.h"
+// The Bullet Physics library
+#include "LinearMath/btVector3.h"
+// The C++ Standard Library
+#include <stdexcept>
+#include <string>;
+
+#include <json/json.h>
+/**
+ * Anonomous namespace so we don't have to declare the config in
+ * the header.
+ */
+namespace
+{
+    /**
+     * Configuration parameters so they're easily accessable.
+     * All parameters must be positive.
+     */
+    const struct Config
+    {
+        double density;
+        double radius;
+        double stiffness;
+        double damping;
+        double triangle_length;
+        double triangle_height;
+        double prism_height;
+        double pretension;     
+    } c =
+   {
+       0.2,     // density (mass / length^3)
+       0.31,     // radius (length)
+       1000.0,   // stiffness (mass / sec^2)
+       10.0,     // damping (mass / sec)
+       10.0,     // triangle_length (length)
+       10.0,     // triangle_height (length)
+       20.0,     // prism_height (length)
+       0.05      // Pretension (percentage)
+  };
+} // namespace
+
+PrismModel::PrismModel() :
+m_pStringController(new PretensionController(c.pretension)),
+tgModel() 
+{
+}
+
+PrismModel::~PrismModel()
+{
+    delete m_pStringController;
+}
+
+void PrismModel::addNodes(tgStructure& tetra,
+                            double edge,
+                            double width,
+                            double height)
+{
+    // bottom right
+    tetra.addNode(-edge / 2.0, 0, 0); // 1
+    // bottom left
+    tetra.addNode( edge / 2.0, 0, 0); // 2
+    // bottom front
+    tetra.addNode(0, 0, width); // 3
+    // top right
+    tetra.addNode(-edge / 2.0, height, 0); // 4
+    // top left
+    tetra.addNode( edge / 2.0, height, 0); // 5
+    // top front
+    tetra.addNode(0, height, width); // 6
+}
+
+void PrismModel::addRods(tgStructure& s)
+{
+    s.addPair( 0,  4, "rod");
+    s.addPair( 1,  5, "rod");
+    s.addPair( 2,  3, "rod");
+}
+
+void PrismModel::addMuscles(tgStructure& s)
+{
+    // Bottom Triangle
+    s.addPair(0, 1,  "muscle");
+    s.addPair(1, 2,  "muscle");
+    s.addPair(2, 0,  "muscle");
+    
+    // Top
+    s.addPair(3, 4, "muscle");
+    s.addPair(4, 5,  "muscle");
+    s.addPair(5, 3,  "muscle");
+
+    //Edges
+    s.addPair(0, 3, "muscle");
+    s.addPair(1, 4,  "muscle");
+    s.addPair(2, 5,  "muscle");
+}
+
+void PrismModel::setup(tgWorld& world)
+{
+    // Define the configurations of the rods and strings
+    const tgRod::Config rodConfig(c.radius, c.density);
+    const tgLinearString::Config muscleConfig(c.stiffness, c.damping);
+    
+    // Create a structure that will hold the details of this model
+    tgStructure s;
+
+    //BEGIN DESERIALIZING
+    
+Json::Value root; // will contains the root value after parsing.
+Json::Reader reader;
+S
+bool parsingSuccessful = reader.parse( FileHelpers::readFil, root );
+if ( !parsingSuccessful )
+{
+// report to the user the failure and their locations in the document.
+std::cout << "Failed to parse configuration\n"
+<< reader.getFormattedErrorMessages();
+return;
+}
+// Get the value of the member of root named 'encoding', return 'UTF-8' if there is no
+// such member.
+std::string encoding = root.get("encoding", "UTF-8" ).asString();
+// Get the value of the member of root named 'encoding', return a 'null' value if
+// there is no such member.
+const Json::Value plugins = root["plug-ins"];
+for ( int index = 0; index < plugins.size(); ++index ) // Iterates over the sequence elements.
+loadPlugIn( plugins[index].asString() );
+setIndentLength( root["indent"].get("length", 3).asInt() );
+setIndentUseSpace( root["indent"].get("use_space", true).asBool() );
+// ...
+// At application shutdown to make the new configuration document:
+// Since Json::Value has implicit constructor for all value types, it is not
+// necessary to explicitly construct the Json::Value object:
+root["encoding"] = getCurrentEncoding();
+root["indent"]["length"] = getCurrentIndentLength();
+root["indent"]["use_space"] = getCurrentIndentUseSpace();
+Json::StyledWriter writer;
+// Make a new JSON document for the configuration. Preserve original comments.
+std::string outputConfig = writer.write( root );
+// You can also use streams. This will put the contents of any JSON
+// stream at a particular sub-value, if you'd like.
+std::cin >> root["subtree"];
+// And you can write to a stream, using the StyledWriter automatically.
+std::cout << root;
+    //END SERIALIZING
+    
+    // Add nodes to the structure
+    addNodes(s, c.triangle_length, c.triangle_height, c.prism_height);
+    
+    // Add rods to the structure
+    addRods(s);
+    
+    // Add muscles to the structure
+    addMuscles(s);
+    
+    // Move the structure so it doesn't start in the ground
+    s.move(btVector3(0, 10, 0));
+    
+    // Create the build spec that uses tags to turn the structure into a real model
+    tgBuildSpec spec;
+    spec.addBuilder("rod", new tgRodInfo(rodConfig));
+    spec.addBuilder("muscle", new tgLinearStringInfo(muscleConfig));
+    
+    // Create your structureInfo
+    tgStructureInfo structureInfo(s, spec);
+
+    // Use the structureInfo to build ourselves
+    structureInfo.buildInto(*this, world);
+
+    // We could now use tgCast::filter or similar to pull out the
+    // models (e.g. muscles) that we want to control. 
+    allMuscles = tgCast::filter<tgModel, tgLinearString> (getDescendants());
+    
+    // Then attach the pretension controller to each of these muscles to keep
+    // the tensegrity's shape
+    for (std::size_t i = 0; i < allMuscles.size(); i++)
+    {
+        allMuscles[i]->attach(m_pStringController);
+    }
+    
+    // Notify controllers that setup has finished.
+    notifySetup();
+    
+    // Actually setup the children
+    tgModel::setup(world);
+}
+
+void PrismModel::step(double dt)
+{
+    // Precondition
+    if (dt <= 0.0)
+    {
+        throw std::invalid_argument("dt is not positive");
+    }
+    else
+    {
+        // Notify observers (controllers) of the step so that they can take action
+        notifyStep(dt);
+        tgModel::step(dt);  // Step any children
+    }
+}
+
+void PrismModel::onVisit(tgModelVisitor& r)
+{
+    // Example: m_rod->getRigidBody()->dosomething()...
+    tgModel::onVisit(r);
+}
+
+const std::vector<tgLinearString*>& PrismModel::getAllMuscles() const
+{
+    return allMuscles;
+}
+    
+void PrismModel::teardown()
+{
+    notifyTeardown();
+    tgModel::teardown();
+}
