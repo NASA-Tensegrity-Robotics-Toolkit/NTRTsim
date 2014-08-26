4b78e4cdc8bab15dfd2b2798e2671f034a25b107
Perry Bhandal <perry.bhandal@gmail.com>
2014-08-26 15:24:53 -0400

    Sample code to read in from a file and print the contents json parsed.

--- src/examples/testjson/testjson.cpp
+++ src/examples/testjson/testjson.cpp
@@ -6,5 +6,18 @@ using namespace std;
 
 int main() {
     cout<<"Hello"<<endl;
-    cout<<FileHelpers::getFileString("data.txt")<<endl;
+    string fileStr = FileHelpers::getFileString("data.txt");
+
+    Json::Value root; // will contains the root value after parsing.
+    Json::Reader reader;
+    bool parsingSuccessful = reader.parse( fileStr, root );
+    if ( !parsingSuccessful )
+    {
+        // report to the user the failure and their locations in the document.
+        std::cout << "Failed to parse configuration\n"
+            << reader.getFormattedErrorMessages();
+        return 1;
+    }
+    string myName = root.get("name", "UTF-8").asString();
+    cout <<"My name is "<<myName<<endl;
 }
