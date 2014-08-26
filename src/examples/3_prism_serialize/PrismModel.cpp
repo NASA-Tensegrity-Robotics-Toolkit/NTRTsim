086c317a0bc21922cbdc3e887cc15aa17fd04d24
Perry Bhandal <perry.bhandal@gmail.com>
2014-08-26 15:50:14 -0400

    Added deserialiation for triangle height.

--- src/examples/3_prism_serialize/PrismModel.cpp
+++ src/examples/3_prism_serialize/PrismModel.cpp
@@ -142,43 +142,21 @@ void PrismModel::setup(tgWorld& world)
 Json::Value root; // will contains the root value after parsing.
 Json::Reader reader;
 S
-bool parsingSuccessful = reader.parse( FileHelpers::readFil, root );
+bool parsingSuccessful = reader.parse( FileHelpers::getFileString("config.json"), root );
 if ( !parsingSuccessful )
 {
 // report to the user the failure and their locations in the document.
 std::cout << "Failed to parse configuration\n"
 << reader.getFormattedErrorMessages();
-return;
+return 1;
 }
 // Get the value of the member of root named 'encoding', return 'UTF-8' if there is no
 // such member.
-std::string encoding = root.get("encoding", "UTF-8" ).asString();
-// Get the value of the member of root named 'encoding', return a 'null' value if
-// there is no such member.
-const Json::Value plugins = root["plug-ins"];
-for ( int index = 0; index < plugins.size(); ++index ) // Iterates over the sequence elements.
-loadPlugIn( plugins[index].asString() );
-setIndentLength( root["indent"].get("length", 3).asInt() );
-setIndentUseSpace( root["indent"].get("use_space", true).asBool() );
-// ...
-// At application shutdown to make the new configuration document:
-// Since Json::Value has implicit constructor for all value types, it is not
-// necessary to explicitly construct the Json::Value object:
-root["encoding"] = getCurrentEncoding();
-root["indent"]["length"] = getCurrentIndentLength();
-root["indent"]["use_space"] = getCurrentIndentUseSpace();
-Json::StyledWriter writer;
-// Make a new JSON document for the configuration. Preserve original comments.
-std::string outputConfig = writer.write( root );
-// You can also use streams. This will put the contents of any JSON
-// stream at a particular sub-value, if you'd like.
-std::cin >> root["subtree"];
-// And you can write to a stream, using the StyledWriter automatically.
-std::cout << root;
-    //END SERIALIZING
+double triangle_height = root.get("triangle_height").asDouble();
+   //END SERIALIZING
     
     // Add nodes to the structure
-    addNodes(s, c.triangle_length, c.triangle_height, c.prism_height);
+    addNodes(s, c.triangle_length, triangle_height, c.prism_height);
     
     // Add rods to the structure
     addRods(s);
