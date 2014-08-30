06c50ffbbcd84fbf841d555b9a60a8ddc7deb294
Perry Bhandal <perry.bhandal@gmail.com>
2014-08-30 16:08:27 -0400

    Have test working on static data.

--- test/helpers/FileHelpers_test.cpp
+++ test/helpers/FileHelpers_test.cpp
@@ -4,6 +4,46 @@
 
 using namespace std;
 
-int main() {
-    cout<<"In test."<<endl;
+namespace {
+
+// The fixture for testing class Foo.
+class FileHelpersTest : public ::testing::Test {
+ protected:
+  // You can remove any or all of the following functions if its body
+  // is empty.
+
+  FileHelpersTest() {
+    // You can do set-up work for each test here.
+  }
+
+  virtual ~FileHelpersTest() {
+    // You can do clean-up work that doesn't throw exceptions here.
+  }
+
+  // If the constructor and destructor are not enough for setting up
+  // and cleaning up each test, you can define the following methods:
+
+  virtual void SetUp() {
+    // Code here will be called immediately after the constructor (right
+    // before each test).
+  }
+
+  virtual void TearDown() {
+    // Code here will be called immediately after each test (right
+    // before the destructor).
+  }
+
+  // Objects declared here can be used by all tests in the test case for Foo.
+};
+
+// Tests that the Foo::Bar() method does Abc.
+TEST_F(FileHelpersTest, MethodBarDoesAbc) {
+  EXPECT_EQ(0, 1);
+}
+
+}  // namespace
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
