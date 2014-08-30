ce46aed9bcf566e58a9fc82b3e77e2c7bf4c024f
Perry Bhandal <perry.bhandal@gmail.com>
2014-08-30 15:40:26 -0400

    Removed core tests, added base for test of FileHelpers.

--- test/core/tgRod_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "core/tgRod.h"
-
-using namespace std;
-
-int main() {
-    cout<<"We're in thi methd."<<endl;
-}
---
+++ test/helpers/FileHelpers_test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "helpers/FileHelpers.h"
+
+using namespace std;
+
+int main() {
+    cout<<"In test."<<endl;
+}
