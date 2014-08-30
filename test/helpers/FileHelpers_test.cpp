4ab83135acd3489c1321082f2682dc7e68cd8b15
Perry Bhandal <perry.bhandal@gmail.com>
2014-08-30 17:33:09 -0400

    Created test for FileHelpers.

--- test/helpers/FileHelpers_test.cpp
+++ test/helpers/FileHelpers_test.cpp
@@ -6,44 +6,44 @@ using namespace std;
 
 namespace {
 
-// The fixture for testing class Foo.
-class FileHelpersTest : public ::testing::Test {
- protected:
-  // You can remove any or all of the following functions if its body
-  // is empty.
+    // The fixture for testing class FileHelpers.
+    class FileHelpersTest : public ::testing::Test {
+        protected:
+            // You can remove any or all of the following functions if its body
+            // is empty.
 
-  FileHelpersTest() {
-    // You can do set-up work for each test here.
-  }
+            FileHelpersTest() {
+                // You can do set-up work for each test here.
+            }
 
-  virtual ~FileHelpersTest() {
-    // You can do clean-up work that doesn't throw exceptions here.
-  }
+            virtual ~FileHelpersTest() {
+                // You can do clean-up work that doesn't throw exceptions here.
+            }
 
-  // If the constructor and destructor are not enough for setting up
-  // and cleaning up each test, you can define the following methods:
+            // If the constructor and destructor are not enough for setting up
+            // and cleaning up each test, you can define the following methods:
 
-  virtual void SetUp() {
-    // Code here will be called immediately after the constructor (right
-    // before each test).
-  }
+            virtual void SetUp() {
+                // Code here will be called immediately after the constructor (right
+                // before each test).
+            }
 
-  virtual void TearDown() {
-    // Code here will be called immediately after each test (right
-    // before the destructor).
-  }
+            virtual void TearDown() {
+                // Code here will be called immediately after each test (right
+                // before the destructor).
+            }
 
-  // Objects declared here can be used by all tests in the test case for Foo.
-};
+            // Objects declared here can be used by all tests in the test case for FileHelpers.
+    };
 
-// Tests that the Foo::Bar() method does Abc.
-TEST_F(FileHelpersTest, MethodBarDoesAbc) {
-  EXPECT_EQ(0, 1);
-}
+    TEST_F(FileHelpersTest, ReadsFilesIntoString) {
+        string fileData = FileHelpers::getFileString("test_file.txt");
+        EXPECT_EQ("test string\n", fileData);
+    }
 
 }  // namespace
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
