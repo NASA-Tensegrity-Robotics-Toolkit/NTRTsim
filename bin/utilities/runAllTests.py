5156359ea26db0ad9bd499d82953756281c48ae5
Perry Bhandal <perry.bhandal@gmail.com>
2014-09-03 12:24:52 -0400

    Completed test runner script.

--- bin/utilities/runAllTests.py
+++ bin/utilities/runAllTests.py
@@ -7,11 +7,22 @@ def isExecutable(filePath):
     return os.path.isfile(filePath) and os.access(filePath, os.X_OK)
 
 def runTest(filePath):
-    print "Asked to run test: %s" % (filePath)
+    return os.system(filePath)
+
+# We set this to true if a single test fails.
+testFailed = False
 
 for root, subFolders, files in os.walk("."):
     for file in files:
         if file.lower().endswith(TEST_SUFFIX):
             filePath = "%s/%s" % (root, file)
             if isExecutable(filePath):
-                runTest(filePath)
+                exitCode = runTest(filePath)
+                if exitCode != 0 and testFailed == False:
+                    # This is our first failure
+                    testFailed = True
+
+if testFailed:
+    exit(1)
+else:
+    exit(0)
