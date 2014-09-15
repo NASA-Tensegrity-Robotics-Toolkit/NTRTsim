d9bcc5a72bf5711fe43e06a2c3c1a0a6b9f5c4b8
Steve Lessard <sr.lessard@gmail.com>
2014-09-15 16:48:29 -0700

    Minor edits

--- src/dev/steve/Escape_T6/logs/printDistances.py
+++ src/dev/steve/Escape_T6/logs/printDistances.py
@@ -1,12 +1,10 @@
 """ Prints the first comma-separated value on every line """
 
-import sys 
-
-f = open(sys.argv[1], 'r') 
+import sys
 
 try:
+    f = open(sys.argv[1], 'r')
     for line in f:
         print line.partition(',')[0] # Implicit '\n' printed as well
 finally:
     f.close()
- 
