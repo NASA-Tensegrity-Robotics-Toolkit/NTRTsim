a56ded7ba56e11ba97aadcd1eabb77ff09893880
Brian <brt6@case.edu>
2014-09-22 17:13:15 -0700

    Updated nodes and pairs for point masses

--- src/dev/btietz/TetraSpineStatic/TetraSpineStaticModel.cpp
+++ src/dev/btietz/TetraSpineStatic/TetraSpineStaticModel.cpp
@@ -55,25 +55,37 @@ namespace
 {
     void addNodes(tgStructure& tetra, double edge, double height)
     {
+		// Front segment
         // right
-        tetra.addNode(-edge / 2.0, 0, 0);
-    // left
-    tetra.addNode( edge / 2.0, 0, 0);
+        tetra.addNode(-edge / 2.0, 0, 0, "base");
+		// left
+		tetra.addNode( edge / 2.0, 0, 0, "base");
         // top
-    tetra.addNode(0, height, 0);
-    // front
-    tetra.addNode(0, height / 2.0, tgUtil::round(std::sqrt(3.0) / 2.0 * height));
-    
+		tetra.addNode(0, height, 0, "base");
+		// front
+		tetra.addNode(0, height / 2.0, tgUtil::round(std::sqrt(3.0) / 2.0 * height), "tip");
+		
+		// Get the next two nodes from existing nodes:
+		tgNodes oldNodes = tetra.getNodes();
+		tgNode nn1 = (oldNodes[0] + oldNodes[2])/2.0; // 4 mid
+		nn1.addTags("PCB");
+		tetra.addNode(nn1);
+		tgNode nn2 = (oldNodes[1] + oldNodes[2])/2.0; // 5 mid
+		nn2.addTags("PCB");
+		tetra.addNode(nn2);
+		
     }
 
     void addPairs(tgStructure& tetra)
     {
         tetra.addPair(0, 1, "back bottom rod");
-    tetra.addPair(0, 2, "back right rod");
-    tetra.addPair(0, 3, "front right rod");
-    tetra.addPair(1, 2, "back left rod");
-    tetra.addPair(1, 3, "front left rod");
-    tetra.addPair(2, 3, "front top rod");
+		tetra.addPair(0, 4, "back rightBottom rod");
+		tetra.addPair(4, 2, "back rightTop rod");
+		tetra.addPair(0, 3, "front right rod");
+		tetra.addPair(1, 5, "back leftBottom rod");
+		tetra.addPair(5, 2, "back leftTop rod");
+		tetra.addPair(1, 3, "front left rod");
+		tetra.addPair(2, 3, "front top rod");
     }
 	
 	
@@ -81,45 +93,38 @@ namespace
              size_t segmentCount)
     {
         const btVector3 offset(0, 0, -21.5);
-    for (size_t i = 0; i < segmentCount; ++i)
-    {
-            /// @todo: the snake is a temporary variable -- will its destructor be called?
-        /// If not, where do we delete its children?
-      tgStructure* const t = new tgStructure(tetra);
-	  //if (i == 0 || i == 1)
-      //{
-		  t->addTags(tgString("mobile segment num", i + 1));
+		for (size_t i = 0; i < segmentCount; ++i)
+		{
+				/// @todo: the snake is a temporary variable -- will its destructor be called?
+			/// If not, where do we delete its children?
+		  tgStructure* const t = new tgStructure(tetra);
+		  t->addTags(tgString("segment num", i + 1)); // Use num0, num1, num2 as a tag!!
 		  
-	/*  }
-	  else
-	  {
-		  t->addTags(tgString("static segment num", i + 1));
-	  }
-	*/
 		  t->move((i + 1) * offset);
 		  
 		  // Add a child to the snake
 		  snake.addChild(t);
-    }
+
+		}
     }
 
     // Add muscles that connect the segments
     void addMuscles(tgStructure& snake)
     {
         const std::vector<tgStructure*> children = snake.getChildren();
-    for (size_t i = 1; i < children.size(); ++i)
-    {
-        tgNodes n0 = children[i-1]->getNodes();
-        tgNodes n1 = children[i  ]->getNodes();
+		for (size_t i = 1; i < children.size(); ++i)
+		{
+			tgNodes n0 = children[i-1]->getNodes();
+			tgNodes n1 = children[i  ]->getNodes();
 
-        snake.addPair(n0[0], n1[0], "outer right muscle");
-        snake.addPair(n0[1], n1[1], "outer left muscle");
-        snake.addPair(n0[2], n1[2], "outer top muscle");
+			snake.addPair(n0[0], n1[0], "outer right muscle");
+			snake.addPair(n0[1], n1[1], "outer left muscle");
+			snake.addPair(n0[2], n1[2], "outer top muscle");
 
-        snake.addPair(n0[0], n1[3], "inner right muscle");
-        snake.addPair(n0[1], n1[3], "inner left muscle");
-        snake.addPair(n0[2], n1[3], "inner top muscle");
-    }
+			snake.addPair(n0[0], n1[3], "inner right muscle");
+			snake.addPair(n0[1], n1[3], "inner left muscle");
+			snake.addPair(n0[2], n1[3], "inner top muscle");
+		}
     }
 
     void mapMuscles(TetraSpineStaticModel::MuscleMap& muscleMap,
@@ -127,12 +132,12 @@ namespace
     {
         // Note that tags don't need to match exactly, we could create
         // supersets if we wanted to
-    muscleMap["inner left"]  = model.find<tgLinearString>("inner left muscle");
-    muscleMap["inner right"] = model.find<tgLinearString>("inner right muscle");
-    muscleMap["inner top"]   = model.find<tgLinearString>("inner top muscle");
-    muscleMap["outer left"]  = model.find<tgLinearString>("outer left muscle");
-    muscleMap["outer right"] = model.find<tgLinearString>("outer right muscle");
-    muscleMap["outer top"]   = model.find<tgLinearString>("outer top muscle");
+		muscleMap["inner left"]  = model.find<tgLinearString>("inner left muscle");
+		muscleMap["inner right"] = model.find<tgLinearString>("inner right muscle");
+		muscleMap["inner top"]   = model.find<tgLinearString>("inner top muscle");
+		muscleMap["outer left"]  = model.find<tgLinearString>("outer left muscle");
+		muscleMap["outer right"] = model.find<tgLinearString>("outer right muscle");
+		muscleMap["outer top"]   = model.find<tgLinearString>("outer top muscle");
     }
 	
 	void addMarkers(tgStructure& structure, TetraSpineStaticModel& model)
@@ -148,7 +153,7 @@ namespace
 		
 		tgNodes n1 = children[1]->getNodes();
 		
-		btRigidBody* secondBody = model.getAllRigids()[6]->getPRigidBody();
+		btRigidBody* secondBody = model.getAllRigids()[9]->getPRigidBody();
 		
 		abstractMarker marker2(secondBody, n1[3] - secondBody->getCenterOfMassPosition (), btVector3(1, 0, 0), 0);
 		
@@ -223,7 +228,7 @@ void TetraSpineStaticModel::setup(tgWorld& world)
     const double friction = 0.8;
     const tgRod::Config rodConfig(radius, density, friction);
     tgBuildSpec spec;
-    spec.addBuilder("mobile rod", new tgRodInfo(rodConfig));
+    spec.addBuilder("rod", new tgRodInfo(rodConfig));
     
     const tgRod::Config staticConfig(radius, 0.0, friction);
     spec.addBuilder("static rod", new tgRodInfo(staticConfig));
