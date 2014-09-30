06ede3b4a17502786c3b21bb704028ae2485e649
Ryan Adams <radams@cyandata.com>
2014-09-30 12:24:14 -0700

    Fixed BASE_DIR path to eliminate non-normalized format (path/to/helpers/../../../)

--- bin/setup/helpers/helper_paths.sh
+++ bin/setup/helpers/helper_paths.sh
@@ -15,8 +15,9 @@
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 # either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
-helper_pwd=`pwd`
-BASE_DIR="${helper_pwd}/../../.."
+
+helper_pwd=$(dirname "${BASH_SOURCE[0]}")
+BASE_DIR="`( cd \"$helper_pwd/../../../\" && pwd )`"
 
 BIN_DIR="${BASE_DIR}/bin"
 CONF_DIR="${BASE_DIR}/conf"
