2438248a399d571dd1521948dc074552a35180d2
Ryan Adams <radams@cyandata.com>
2014-09-29 20:51:22 -0700

    Updated helper definitions to correctly determine number of processors on OS X; Added copyright and functionality to setup_virtualenv.sh

--- bin/setup/setup_virtualenv.sh
+++ bin/setup/setup_virtualenv.sh
@@ -1,15 +1,56 @@
 #!/bin/bash
 
-exit 1  # This is just a sketch. Get it working before removing this...
+# Copyright © 2012, United States Government, as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All rights reserved.
+# 
+# The NASA Tensegrity Robotics Toolkit (NTRT) v1 platform is licensed
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0.
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
 
-# Download virtualenv.py (or maybe just have it in helpers?)
-# curl -L -o virtualenv.py https://raw.github.com/pypa/virtualenv/master/virtualenv.py
+# Purpose: Boost library Setup
+# Date:    2013-05-01
+
+##############################################################################
+#                         START DO NOT MODIFY                                #
+##############################################################################
+SCRIPT_PATH="`dirname \"$0\"`"
+SCRIPT_PATH="`( cd \"$SCRIPT_PATH\" && pwd )`"
+
+# Add the relative path from this script to the helpers folder.
+pushd "${SCRIPT_PATH}/helpers/" > /dev/null
+
+if [ ! -f "helper_functions.sh" ]; then
+    echo "Could not find helper_functions.sh. Are we in the bash helpers folder?"
+    exit 1;
+fi
+
+
+# Import our common files
+source "helper_functions.sh"
+source "helper_paths.sh"
+source "helper_definitions.sh"
+
+# Get out of the bash helpers folder.
+popd > /dev/null
+##############################################################################
+#                          END DO NOT MODIFY                                 #
+##############################################################################
+
 
 # Call virtualenv.py with --no-setuptools to create a new virtualenv
-python virtualenv.py -p /usr/bin/python2.7 --no-setuptools venv2
+python "$SETUP_DIR/helpers/virtualenv.py" --no-setuptools "$ENV_DIR/venv"
 
 # Activate the new virtualenv
-# source $venv/bin/activate (?)
+source "$ENV_DIR/venv/bin/activate"
 
 # Install pip
 easy_install pip
