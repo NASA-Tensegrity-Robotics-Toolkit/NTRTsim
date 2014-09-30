84b9f757ac6975ed4addd2dc58d9bac7f2c011c7
Brian <brt6@case.edu>
2014-09-29 22:21:48 -0700

    Parameter changes for sine waves

--- src/dev/btietz/hardwareSineWaves/AppTetraSpineHardwareSine.cpp
+++ src/dev/btietz/hardwareSineWaves/AppTetraSpineHardwareSine.cpp
@@ -56,7 +56,7 @@ int main(int argc, char** argv)
     // Second create the view
     const double stepSize = 1.0/1000.0; // Seconds
     const double renderRate = 1.0/60.0; // Seconds
-    tgSimViewGraphics view(world, stepSize, renderRate);
+    tgSimView view(world, stepSize, renderRate);
 
     // Third create the simulation
     tgSimulation simulation(view);
