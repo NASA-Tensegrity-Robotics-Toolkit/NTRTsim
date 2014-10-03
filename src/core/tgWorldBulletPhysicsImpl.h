58b5a1e7ff884e213e7b35f1dfaa3d699ec6d8cd
Brian <brt6@case.edu>
2014-10-03 12:53:08 -0700

    Documentation for NULL ground

--- src/core/tgWorldBulletPhysicsImpl.h
+++ src/core/tgWorldBulletPhysicsImpl.h
@@ -54,6 +54,8 @@ class tgWorldBulletPhysicsImpl : public tgWorldImpl
   /** 
    * The only constructor.
    * @param[in] gravity the gravitational acceleration in m/sec^2
+   * @param[in] ground - a container class that holds a rigid body and
+   * collsion object for the ground. A NULL pointer results in no ground.
    */
   tgWorldBulletPhysicsImpl(const tgWorld::Config& config,
                            tgBulletGround* ground);
