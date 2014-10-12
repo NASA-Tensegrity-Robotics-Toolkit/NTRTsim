155ef650991d801ed249dcf92f592d09a0433bcc
Ryan Adams <radams@cyandata.com>
2014-10-11 17:28:29 -0700

    Added Weather Update server example

---
+++ src/dev/radams/zeromq/wuclient.cpp
@@ -0,0 +1,47 @@
+
+
+//
+// Weather update client in C++
+// Connects SUB socket to tcp://localhost:5556
+// Collects weather updates and finds avg temp in zipcode
+//
+// Olivier Chamoux <>
+//
+#include <zmq.hpp>
+#include <iostream>
+#include <sstream>
+
+int main (int argc, char *argv[])
+{
+zmq::context_t context (1);
+
+// Socket to talk to server
+std::cout << "Collecting updates from weather server…\n" << std::endl;
+zmq::socket_t subscriber (context, ZMQ_SUB);
+subscriber.connect("tcp://localhost:5556");
+
+// Subscribe to zipcode, default is NYC, 10001
+const char *filter = (argc > 1)? argv [1]: "10001 ";
+subscriber.setsockopt(ZMQ_SUBSCRIBE, filter, strlen (filter));
+
+// Process 100 updates
+int update_nbr;
+long total_temp = 0;
+for (update_nbr = 0; update_nbr < 100; update_nbr++) {
+
+zmq::message_t update;
+int zipcode, temperature, relhumidity;
+
+subscriber.recv(&update);
+
+std::istringstream iss(static_cast<char*>(update.data()));
+iss >> zipcode >> temperature >> relhumidity ;
+
+total_temp += temperature;
+}
+std::cout << "Average temperature for zipcode '"<< filter
+<<"' was "<<(int) (total_temp / update_nbr) <<"F"
+<< std::endl;
+return 0;
+}
+
---
+++ src/dev/radams/zeromq/wuserver.cpp
@@ -0,0 +1,45 @@
+
+
+//
+// Weather update server in C++
+// Binds PUB socket to tcp://*:5556
+// Publishes random weather updates
+//
+// Olivier Chamoux <>
+//
+#include <zmq.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define within(num) (int) ((float) num * random () / (RAND_MAX + 1.0))
+
+int main () {
+
+// Prepare our context and publisher
+zmq::context_t context (1);
+zmq::socket_t publisher (context, ZMQ_PUB);
+publisher.bind("tcp://*:5556");
+publisher.bind("ipc://weather.ipc");
+
+// Initialize random number generator
+srandom ((unsigned) time (NULL));
+while (1) {
+
+int zipcode, temperature, relhumidity;
+
+// Get values that will fool the boss
+zipcode = within (100000);
+temperature = within (215) - 80;
+relhumidity = within (50) + 10;
+
+// Send message to all subscribers
+zmq::message_t message(20);
+snprintf ((char *) message.data(), 20 ,
+"%05d %d %d", zipcode, temperature, relhumidity);
+publisher.send(message);
+
+}
+return 0;
+}
+
