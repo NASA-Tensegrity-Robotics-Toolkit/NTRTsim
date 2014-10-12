782a8674cbc0895e44e7dd471f469f93c19edc84
Ryan Adams <radams@cyandata.com>
2014-10-11 17:15:09 -0700

    Added working ZeroMQ 'Hello World' example

---
+++ src/dev/radams/zeromq/helloWorldServer.cpp
@@ -0,0 +1,44 @@
+
+
+//
+// Hello World server in C++
+// Binds REP socket to tcp://*:5555
+// Expects "Hello" from client, replies with "World"
+//
+#include <zmq.hpp>
+#include <string>
+#include <iostream>
+#ifndef _WIN32
+#include <unistd.h>
+#else
+#include <windows.h>
+#endif
+
+int main () {
+// Prepare our context and socket
+zmq::context_t context (1);
+zmq::socket_t socket (context, ZMQ_REP);
+socket.bind ("tcp://*:5555");
+
+while (true) {
+zmq::message_t request;
+
+// Wait for next request from client
+socket.recv (&request);
+std::cout << "Received Hello" << std::endl;
+
+// Do some 'work'
+#ifndef _WIN32
+sleep(1);
+#else
+Sleep (1);
+#endif
+
+// Send reply back to client
+zmq::message_t reply (5);
+memcpy ((void *) reply.data (), "World", 5);
+socket.send (reply);
+}
+return 0;
+}
+
