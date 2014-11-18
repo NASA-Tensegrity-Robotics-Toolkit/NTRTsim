3077812d5f0ed22d8f25fdbf19b8db59480131dd
brian <brt6@case.edu>
2014-11-18 15:11:04 -0500

    Comments for muscleAnchor, returning a reference for force instead of a copy

--- src/core/muscleAnchor.h
+++ src/core/muscleAnchor.h
@@ -23,6 +23,7 @@
  * @file MuscleAnchor.h
  * @brief Definitions of class MuscleAnchor.
  * @author Brian Mirletz and Atil Iscen
+ * @date November 2014
  * $Id$
  */
 
@@ -33,16 +34,37 @@
 #include <string>
 #include <utility> //std::pair
 
+// Forward References
 class btRigidBody;
 class btPersistentManifold;
 class MuscleNP;
 
+/**
+ * A class that allows Muscle2P and MuscleNP to attach to btRigidBodies
+ * Anchors track a specific point on a body as that body translates
+ * and rotates. They can either be 'non-sliding' which typically means
+ * a pin jointed anchor (and are typically permanent), or sliding,
+ * which means they track a specific contact point within a
+ * btPersistentManifold. 
+ */
 class muscleAnchor
 {
 public:
 	// MuscleNP needs to scale the forces
    friend class MuscleNP;
-
+	
+	/**
+	 * The only constructor. At a minimum requires a body and a position
+	 * on that body to track. Sliding anchors require additional data
+	 * @param[in] body - a pointer to the btRigidBody this is attached to
+	 * @param[in] pos - The position in world coordinates where this attaches
+	 * @param[in] cn - A btVector3 that specifies the direction of contact
+	 * Only requried for sliding anchors
+	 * @param[in] perm - Whether or not this anchor can be deleted in
+	 * the middle of a simulation
+	 * @param[in] slide - Whether this represents a pin joint or a sliding contact
+	 * @param[in] m - a btPersistenManifold that is used to track contacts
+	 */
     muscleAnchor(btRigidBody *body, 
 					btVector3 pos,
 					btVector3 cn = btVector3(0.0, 0.0, 0.0),
@@ -50,31 +72,74 @@ public:
 					bool slide = false,
 					btPersistentManifold* m = NULL);
     
+    /**
+     * Destructor, nothing to delete
+     */
     ~muscleAnchor();
     
+    /**
+     * Return the current position of the anchor in world coordinates
+     * Uses attachedRelativeOriginalPosition and the attachedBody's
+     * btTransform
+     */
     btVector3 getWorldPosition() const;
 	
-	// Bool returns if this point is actually on the body
+	/**
+	 * Update attachedRelativeOriginalPosition based on the sliding
+	 * of the string. This also checks if the new sliding position
+	 * is still on the body.
+	 * @return bool returns if this point is actually on the body. The
+	 * body should be deleted if this returns false
+	 */
 	bool setWorldPosition(btVector3& newPos);
 	
-    // Relative to the body
+    /**
+     * Get the position of the point in body coordinaates
+     * @return a btVector3 in body coordinates
+     */
     btVector3 getRelativePosition() const;
     
+    /**
+     * Return an up to date contact normal based on the rigid
+     * body's btTransform
+     * @return the contact normal, accounting for any rotation from
+     * when the body was first contacted.
+     */
     btVector3 getContactNormal() const;
     
+    /**
+     * Update our manifold pointer. This memory is often reassigned
+     * so the new manifold is accepted if our old manifold no longer
+     * contains our rigid body. It is also accepted if its contact point
+     * is closer than our manifold's
+     * @return a bool that is true if the new manifold was accepted
+     */
     bool updateManifold(btPersistentManifold* m);
 	
+	/**
+	 * Return a pointer to our btPersistentManifold (typically so 
+	 * something can compare getManifoldDistance with a new manifold)
+	 */
 	btPersistentManifold* getManifold() const
 	{
 		return manifold;
 	}
 	
-	btVector3 getForce() const
+	/**
+	 * @return a const reference to the force we just applied (or are 
+	 * about to apply)
+	 */
+	const btVector3& getForce() const
 	{
 		return force;
 	}
 
-	// Address should never be changed, body is not const
+	/**
+	 * The rigid body we affect
+	 * Address should never be changed, body is not const
+	 * @todo Create applyForce functions so this doesn't have to
+	 * be exposed
+     */
     btRigidBody * const attachedBody;
 	
 	/// @todo remove this. Anchors and sensors should be seperate classes
@@ -94,20 +159,41 @@ public:
      */
     const bool sliding;
     
+    /**
+     * A pair of the distance between the current world position and 
+     * this manifolds contact point, as well as the contact normal
+     * of this manifold
+     * @return distance to the contact, contact normal 
+     */
     std::pair<btScalar, btVector3> getManifoldDistance(btPersistentManifold* m) const;
     
 private:
 
 	
-	 // Relative to the body when it is first constructed
+	/**
+	 * The relative world position, stored by multiplying by
+	 * the inverse transform of the initial orientation
+	 */
     btVector3 attachedRelativeOriginalPosition;
 	
+	/**
+	 * For sliding anchors: the normal between the relevent two
+	 * btCollisionObjects. Used in determining validity of anchors and
+	 * applying forces to rigid bodies
+	 */
 	btVector3 contactNormal;
 	
-	// Not const, bullet owns this
+	/**
+	 * The manifold that generated this body if it is a sliding contact
+	 * NULL if its a pin joint
+	 * Not const, bullet owns this, and we update it as best we can
+	 */
 	btPersistentManifold* manifold;
 	
-	// Store force so we can normalize it on a per-body basis
+	/**
+	 * Store force so we can normalize it appropreately. Set and
+	 * accessed directly by MuscleNP
+	 */
 	btVector3 force;
 	
 };
