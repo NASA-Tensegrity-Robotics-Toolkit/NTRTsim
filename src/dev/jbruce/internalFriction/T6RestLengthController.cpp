cbfa4400dd72931f0132483eca299f825b185689
Jonathan <jbruce@soe.ucsc.edu>
2014-11-19 13:17:40 -0800

    Working on getting angles as an output to terminal in internalFriction app

--- src/dev/jbruce/internalFriction/T6RestLengthController.cpp
+++ src/dev/jbruce/internalFriction/T6RestLengthController.cpp
@@ -50,19 +50,7 @@ void T6RestLengthController::onSetup(T6Model& subject)
 {
     // Do a one-time update of all cable rest lengths
     // First, get all muscles (cables)
-//    const std::vector<tgLinearString*> muscles = subject.getAllMuscles();
-//
-//    // then, iterate over all muscles
-//    for (size_t i = 0; i < muscles.size(); ++i)
-//    {
-//        tgLinearString * const pMuscle = muscles[i];
-//	assert(pMuscle != NULL);
-//
-//	double desiredRestLength = pMuscle->getStartLength() - m_restLengthDiff;
-//	// Note that the single step version of setRestLength is used here,
-//	// since we only want to call it once (not iteratively like the original.)
-//	pMuscle->setRestLengthSingleStep(desiredRestLength);
-//    }
+
 	const std::vector<tgLinearString*> passiveMuscles = subject.getPassiveMuscles();
 	const std::vector<tgLinearString*> activeMuscles = subject.getActiveMuscles();
 
@@ -90,77 +78,33 @@ void T6RestLengthController::onSetup(T6Model& subject)
 void T6RestLengthController::onStep(T6Model& subject, double dt)
 {
 	static int count = 0;
-	const std::vector<tgLinearString*> muscles = subject.getAllMuscles();
+	const std::vector<tgLinearString*> strings = subject.getAllMuscles();
+	Muscle2P* muscles;
 
 	if(count > 100)
 	{
-		for(size_t i=0; i<muscles.size(); i++)
+		for(size_t i=0; i<strings.size(); i++)
 		{
-			std::cout << (muscles[i]->getTension())/10 << "\t";
+			std::cout << (strings[i]->getTension())/10 << "\t";
 		}
 		std::cout << "\n";
-		for(size_t i=0; i<muscles.size(); i++)
+		for(size_t i=0; i<strings.size(); i++)
 			{
-				std::cout << (muscles[i]->getCurrentLength())/10 << "\t";
+				std::cout << (strings[i]->getCurrentLength())/10 << "\t";
 			}
-			std::cout << "\n";
+		std::cout << "\n";
+		for(size_t i=0; i<strings.size(); i++)
+			muscles = strings[i]->getMuscle();
+			{
+				std::cout << muscles-> << "\t";
+			}
+		std::cout << "\n";
 	count = 0;
 	}
 	else
 	{
 		count++;
 	}
-    /*if (dt <= 0.0)
-    {
-        throw std::invalid_argument("dt is not positive");
-    }
-    else
-    {
-        // Do an update of all cable rest lengths
-        // First, get all muscles (cables)
-        const std::vector<tgLinearString*> muscles = subject.getAllMuscles();
 
-	// then, iterate over all muscles
-	for (size_t i = 0; i < muscles.size(); ++i)
-	{
-	    tgLinearString * const pMuscle = muscles[i];
-	    assert(pMuscle != NULL);
-
-	    // set rest length of the i-th muscle
-	    double desiredRestLength = pMuscle->getStartLength() - m_restLengthDiff;
-	    pMuscle->setRestLength(desiredRestLength, dt);
-	}
-    }*/
-	/*if (dt <= 0.0)
-	{
-		throw std::invalid_argument("dt is not positive");
-	}
-	else
-	{
-		// Do an update of all cable rest lengths
-		// First, get all muscles (cables)
-		const std::vector<tgLinearString*> passiveMuscles = subject.getPassiveMuscles();
-		const std::vector<tgLinearString*> activeMuscles = subject.getActiveMuscles();
-
-		// then, iterate over all muscles
-		for (size_t i = 0; i < passiveMuscles.size(); ++i)
-		{
-//			tgLinearString * const pMuscle = passiveMuscles[i];
-			assert(passiveMuscles[i] != NULL);
-
-			// set rest length of the i-th muscle
-			double desiredRestLength = passiveMuscles[i]->getRestLength();
-			passiveMuscles[i]->setRestLength(desiredRestLength, dt);
-		}
-		for (size_t i = 0; i < activeMuscles.size(); ++i)
-		{
-//			tgLinearString * const pMuscle = activeMuscles[i];
-			assert(activeMuscles[i] != NULL);
-
-			// set rest length of the i-th muscle
-			double desiredRestLength = activeMuscles[i]->getRestLength()*m_controllerMuscleRatio;
-			activeMuscles[i]->setRestLength(desiredRestLength, dt);
-		}
-	}*/
       // Nothing!!
 }
