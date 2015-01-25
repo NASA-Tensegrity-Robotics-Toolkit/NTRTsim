03942ac3aa8c5b9c926de064c65fb8105e9f30bc
Perry Bhandal <perry.bhandal@gmail.com>
2015-01-25 07:30:25 -0500

    Added tests for Directory listing based on filters.

--- bin/python_scripts/src/utilities/directory.py
+++ bin/python_scripts/src/utilities/directory.py
@@ -20,23 +20,17 @@ class Directory:
 
         extWhiteList: Optional. If specified, only files which end with a path in extWhiteList will be returned.
         """
-        rawList = os.listdir(self.absPath)
-        whiteListed = []
-        removedDirs = []
+        rawList = [ f for f in os.listdir(self.absPath) if os.path.isfile(os.path.join(self.absPath,f)) ]
 
         # Remove blacklisted extensions (if needed)
         if len(extWhiteList) > 0:
+            whiteListed = []
+
             for entry in rawList:
                 for whiteFilter in extWhiteList:
                     if entry.endswith(whiteFilter):
                         whiteListed.append(entry)
 
-        # Remove folders.
-        for entry in whiteListed:
-            if os.path.isfile("%s/%s" % (self.absPath, entry)):
-                print "%s is a file." % entry
-                removedDirs.append(entry)
-            else:
-                print "%s is a folder." % entry
-
-        return removedDirs
+            return whiteListed
+        else:
+            return rawList
--- bin/python_scripts/test/utilities/test_directory.py
+++ bin/python_scripts/test/utilities/test_directory.py
@@ -4,45 +4,24 @@ from src.utilities.directory import *
 
 class TestDirectory(unittest.TestCase):
 
-    DEFAULT_LIST_NO_FOLDERS = ["bullet.ini.default", "test.ini", "ini.bullet", "boost.ini.default", "test.txt"]
-
-    DEFAULT_LIST_HAS_FOLDERS = ["bullet.ini.default", "default", "folder.ini.default", "test.ini", "ini.bullet", "boost.ini.default", "test.txt"]
+    DEFAULT_LIST = ["bullet.ini.default", "folder", "folder.ini" "test.ini", "ini.bullet", "boost.ini.default", "test.txt"]
 
     DIR_ABS_PATH = "/home/perry/work"
 
     def setUp(self):
-        flexmock(os).should_receive('listdir').and_return(self.DEFAULT_LIST_NO_FOLDERS)
+        flexmock(os).should_receive('listdir').and_return(self.DEFAULT_LIST)
+        flexmock(os.path).should_receive('isfile').and_return(True)
         self.dirUnderTest = Directory(self.DIR_ABS_PATH)
 
-    def __setListDirReturn(self, toReturn):
-        flexmock(os).should_receive('listdir').and_return(toReturn)
-
     def testGetDirNoFilter(self):
-        self.__setListDirReturn(self.DEFAULT_LIST_NO_FOLDERS)
-        self.assertEqual(self.dirUnderTest.getDirList(), self.DEFAULT_LIST_NO_FOLDERS)
+        self.assertEqual(self.dirUnderTest.getDirList(), self.DEFAULT_LIST)
 
     def testGetDirOneFilter(self):
-        self.__setListDirReturn(self.DEFAULT_LIST_NO_FOLDERS)
         filteredList = ["bullet.ini.default", "boost.ini.default"]
         self.assertEqual(self.dirUnderTest.getDirList([".ini.default"]), filteredList)
 
     def testGetDirTwoFilter(self):
-        self.__setListDirReturn(self.DEFAULT_LIST_NO_FOLDERS)
         filteredList = ["bullet.ini.default", "ini.bullet", "boost.ini.default"]
         self.assertEqual(self.dirUnderTest.getDirList([".ini.default", ".bullet"]), filteredList)
 
-    def testIgnoreFoldersNoFilter(self):
-        # Create the expected array.
-        expectedDir = ["bullet.ini.default", "test.ini", "ini.bullet", "boost.ini.default", "test.txt"]
-
-        flexmock(os.path).should_receive('isfile').with_args("%s/default" % self.DIR_ABS_PATH).and_return(False)
-        flexmock(os.path).should_receive('isfile').with_args("%s/folder.ini.default" % self.DIR_ABS_PATH).and_return(False)
-
-        for notDir in expectedDir:
-            flexmock(os.path).should_receive('isfile').with_args("%s/%s" % (self.DIR_ABS_PATH, notDir)).and_return(True)
-
-        # Set return type to return default list with folders.
-        self.__setListDirReturn(self.DEFAULT_LIST_HAS_FOLDERS)
-
-        # Check if the dir list is equal to what we expected.
-        self.assertEqual(self.dirUnderTest.getDirList(), expectedDir)
+    #TODO: Add a test here for folder removal.
