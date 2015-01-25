3bb0b1d0a154048119670270fefd8fcfa0cf77de
Perry Bhandal <perry.bhandal@gmail.com>
2015-01-25 15:35:39 -0500

    Added common helpers.

--- bin/python_scripts/test/configuration/test_config_generator.py
+++ bin/python_scripts/test/configuration/test_config_generator.py
@@ -12,25 +12,28 @@ class TestConfigGenerator(unittest.TestCase):
     def setUp(self):
         self.configGenerator = ConfigGenerator(self.CONF_GENERATED_DIR, self.CONF_DEFAULT_DIR)
 
+    def __setDirListReturn(self, generatedList):
+        flexmock(Directory).should_receive('getDirList').and_return(generatedList).and_return(self.DEFAULT_LIST)
+
     def testNoMissing(self):
         generatedList = ["bullet.ini", "gmocktest.ini", "general.ini"]
-        flexmock(Directory).should_receive('getDirList').and_return(generatedList).and_return(self.DEFAULT_LIST)
+        self.__setDirListReturn(generatedList)
         self.assertEqual(self.configGenerator.getMissing(), [])
 
     def testTwoMissing(self):
         generatedList = ["gmocktest.ini"]
-        flexmock(Directory).should_receive('getDirList').and_return(generatedList).and_return(self.DEFAULT_LIST)
+        self.__setDirListReturn(generatedList)
         self.assertEqual(self.configGenerator.getMissing(), ["bullet.ini.default" ,"general.ini.default"])
 
     def testGenerateNoConf(self):
         generatedList = ["bullet.ini", "gmocktest.ini", "general.ini"]
-        flexmock(Directory).should_receive('getDirList').and_return(generatedList).and_return(self.DEFAULT_LIST)
+        self.__setDirListReturn(generatedList)
         flexmock(shutil).should_receive('copy').times(0)
         self.configGenerator.generateMissing()
 
     def testGenerateTwoConf(self):
         generatedList = ["general.ini"]
-        flexmock(Directory).should_receive('getDirList').and_return(generatedList).and_return(self.DEFAULT_LIST)
+        self.__setDirListReturn(generatedList)
         flexmock(shutil).should_receive('copy').times(2)
         self.configGenerator.generateMissing()
 
