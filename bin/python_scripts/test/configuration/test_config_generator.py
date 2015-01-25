dec331779392fd8589af728b95f5542dbb531827
Perry Bhandal <perry.bhandal@gmail.com>
2015-01-25 08:13:00 -0500

    Added test for two file generation.

--- bin/python_scripts/test/configuration/test_config_generator.py
+++ bin/python_scripts/test/configuration/test_config_generator.py
@@ -6,8 +6,18 @@ class TestConfigGenerator(unittest.TestCase):
 
     DEFAULT_LIST = ["bullet.ini.default", "gmocktest.ini.default", "general.ini.default"]
 
+    CONF_GENERATED_DIR = "conf"
+    CONF_DEFAULT_DIR = "conf/default"
+
+    def setUp(self):
+        self.configGenerator = ConfigGenerator(self.CONF_GENERATED_DIR, self.CONF_DEFAULT_DIR)
+
     def testGenerateNoConf(self):
-        configGenerator = ConfigGenerator("conf", "conf/default")
         generatedList = ["bullet.ini", "gmocktest.ini", "general.ini"]
         flexmock(Directory).should_receive('getDirList').and_return(generatedList).and_return(self.DEFAULT_LIST)
-        self.assertEqual(configGenerator.getMissing(), [])
+        self.assertEqual(self.configGenerator.getMissing(), [])
+
+    def testGenerateTwoConf(self):
+        generatedList = ["gmocktest.ini"]
+        flexmock(Directory).should_receive('getDirList').and_return(generatedList).and_return(self.DEFAULT_LIST)
+        self.assertEqual(self.configGenerator.getMissing(), ["bullet.ini.default" ,"general.ini.default"])
