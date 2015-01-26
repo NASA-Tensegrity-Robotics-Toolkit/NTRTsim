552870bc669bd824a6c263758fc47fe4e3045791
Perry Bhandal <perry.bhandal@gmail.com>
2015-01-26 12:17:01 -0500

    Finished tests for downloader.

--- bin/python_scripts/test/download/test_downloader.py
+++ bin/python_scripts/test/download/test_downloader.py
@@ -8,18 +8,42 @@ class TestDownloader(unittest.TestCase):
     LOCAL_SAVE_PATH = "/home/perry/work/git/NTRTsim/env/downloads"
     LOCAL_SAVE_NAME = "myFile.tar.gz"
 
+    def setUp(self):
+        self.downloader = Downloader(self.URL, self.LOCAL_SAVE_PATH, self.LOCAL_SAVE_NAME)
+
     def testGetErrorUrlOpen(self):
         flexmock(urllib2).should_receive('urlopen').and_raise(urllib2.URLError("Error message"))
 
-        downloader = Downloader(self.URL, self.LOCAL_SAVE_PATH, self.LOCAL_SAVE_NAME)
 
         with self.assertRaises(DownloaderError):
-            downloader.attemptDownload()
-
-    def testIOErrorWriteToIsNone(self):
-        pass
+            self.downloader.attemptDownload()
 
     def testIOErrorWriteToIsNotNoneVerifyCloseCall(self):
-        pass
+        fileMock = flexmock()
+        urlMock = flexmock()
+
+        urlMock.should_receive('read').and_raise(IOError("error message.")).once().ordered()
+        fileMock.should_receive('close').once().ordered()
+
+        flexmock(urllib2).should_receive('urlopen').and_return(urlMock)
+        flexmock(FileUtils).should_receive('open').and_return(fileMock)
+
+        with self.assertRaises(DownloaderError):
+            self.downloader.attemptDownload()
 
 
+    def testFinishReadNoErrorVerifyCloseCall(self):
+        readString = "bunchofchars"
+
+        fileMock = flexmock()
+        urlMock = flexmock()
+
+        fileMock.should_receive('write').with_args(readString).once().ordered()
+        fileMock.should_receive('close').once().ordered()
+
+        urlMock.should_receive('read').and_return("bunchofchars").and_return(None).twice()
+
+        flexmock(urllib2).should_receive('urlopen').and_return(urlMock)
+        flexmock(FileUtils).should_receive('open').and_return(fileMock)
+
+        self.downloader.attemptDownload()
