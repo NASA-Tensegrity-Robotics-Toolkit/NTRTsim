3a7b1e2177caa486990e3420d7f5e70ec48e99ed
Perry Bhandal <perry.bhandal@gmail.com>
2015-05-21 13:27:34 -0400

    Added tests for slashes.

--- scripts/learning/tests/utils/test_resource_path.py
+++ scripts/learning/tests/utils/test_resource_path.py
@@ -5,6 +5,21 @@ from utils import ResourcePath, ResourcePathError
 class TestResourcePath(unittest.TestCase):
 
     def testRootPathNoTrailingSlashThrowsError(self):
-
         with self.assertRaises(ResourcePathError):
             ResourcePath("test/meow")
+
+    def testRootPathHasTrailingSlashNoError(self):
+        self.__getResPathObject()
+
+    def testGetPathHasForwardSlashThrowsError(self):
+        resPath = self.__getResPathObject()
+
+        with self.assertRaises(ResourcePathError):
+            resPath.getResourcePath("/relativePath")
+
+    def testGetPathNoForwardSlash(self):
+        resPath = self.__getResPathObject()
+        resPath.getResourcePath("relativePath")
+
+    def __getResPathObject(self):
+        return ResourcePath("test/meow/")
