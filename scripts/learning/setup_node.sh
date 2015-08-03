ca660e79bfc22d3dff60944d52e87661c2524238
Perry Bhandal <perry.bhandal@gmail.com>
2015-08-03 15:10:20 -0400

    Added learning node setup script.

---
+++ scripts/learning/setup_node.sh
@@ -0,0 +1,14 @@
+apt-get install g++ libglib2.0-dev curl freeglut3 freeglut3-dev cmake build-essential unzip git rsync python-pip python-dev screen
+
+git clone https://github.com/NASA-Tensegrity-Robotics-Toolkit/NTRTsim.git
+
+cd NTRTsim
+
+./setup.sh
+./setup.sh
+
+cd bin
+
+./build.sh
+
+pip install psutil
