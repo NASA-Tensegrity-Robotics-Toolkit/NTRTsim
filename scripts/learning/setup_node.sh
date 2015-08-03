534effa673b47e53621dbc4b74a375daed7e63df
Perry Bhandal <perry.bhandal@gmail.com>
2015-08-03 15:16:30 -0400

    Added auto-yes on apt-get.

--- scripts/learning/setup_node.sh
+++ scripts/learning/setup_node.sh
@@ -1,4 +1,4 @@
-apt-get install g++ libglib2.0-dev curl freeglut3 freeglut3-dev cmake build-essential unzip git rsync python-pip python-dev screen
+apt-get -y install g++ libglib2.0-dev curl freeglut3 freeglut3-dev cmake build-essential unzip git rsync python-pip python-dev screen
 
 git clone https://github.com/NASA-Tensegrity-Robotics-Toolkit/NTRTsim.git
 
