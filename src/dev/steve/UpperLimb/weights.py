f6bd2b898f9a0fe9048edca2c1fdaff09521838a
Steven Lessard <sr.lessard@gmail.com>
2015-08-31 11:23:06 -0700

    Made nWeights variable with respect to input, hidden, and output nodes

--- src/dev/steve/UpperLimb/weights.py
+++ src/dev/steve/UpperLimb/weights.py
@@ -3,7 +3,10 @@
 import random         
 import sys
 
-nWeights = 250
+nInputs = 3
+nHidden = 10
+nOutputs = 22
+nWeights = (nInputs+1)*nHidden + (nHidden+1)*nOutputs # follow nnv2 format
 
 for i in range (0,nWeights):
     sys.stdout.write("%f" % ((random.random()*2)-1))
