b245791d07b23d96706dcc3343f4d77c197c159f
Dawn <dhustigs@ucsc.edu>
2015-09-21 12:33:26 -0700

    Added an exeception to make sure that population size equals number of trials when starting Monte Carlo.

--- scripts/learning/src/evolution/evolution_job_master.py
+++ scripts/learning/src/evolution/evolution_job_master.py
@@ -245,6 +245,8 @@ class EvolutionJobMaster(NTRTJobMaster):
         and then returns a new set of parameters based on the specification file
         """
 
+	numTrials = self.jConf['learningParams']['numTrials']
+
         params = self.jConf["learningParams"][paramName]
 
         useAvg = params['useAverage']
@@ -278,6 +280,8 @@ class EvolutionJobMaster(NTRTJobMaster):
 
             # Fill in remaining population with random parameters
             for i in range(len(nextGeneration), params['populationSize']):
+		if(params['monteCarlo'] and params['populationSize'] != numTrials):
+	            raise NTRTMasterError("Number of trials must equal population size!")
                 if (i < 0):
                     raise NTRTMasterError("Number of controllers greater than population size!")
 
