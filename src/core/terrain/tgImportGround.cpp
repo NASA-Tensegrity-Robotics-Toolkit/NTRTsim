a0519f2ab534fcef636580820cf3986703a8d7e5
Edward Zhu <zhu.edward445@gmail.com>
2016-06-17 16:55:39 -0700

    First commit of tgImportGround. Files were copied from tgHillyGround. No changes made yet.

---
+++ src/core/terrain/tgImportGround.cpp
@@ -0,0 +1,213 @@
+/**
+ * Copyright © 2012, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ * 
+ * The NASA Tensegrity Robotics Toolkit (NTRT) v1 platform is licensed
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+/**
+ * @file tgImportGround.cpp
+ * @brief Contains the implementation of class tgImportGround
+ * @author Steven Lessard
+ * $Id$
+ */
+
+//This Module
+#include "tgImportGround.h"
+
+//Bullet Physics
+#include "BulletCollision/CollisionShapes/btBoxShape.h"
+#include "BulletCollision/CollisionShapes/btTriangleIndexVertexArray.h"
+#include "BulletCollision/CollisionShapes/btBvhTriangleMeshShape.h"
+#include "BulletDynamics/Dynamics/btRigidBody.h"
+#include "LinearMath/btDefaultMotionState.h"
+#include "LinearMath/btTransform.h"
+
+// The C++ Standard Library
+#include <cassert>
+#include <iostream>
+
+tgImportGround::Config::Config(btVector3 eulerAngles,
+        double friction,
+        double restitution,
+        btVector3 size,
+        btVector3 origin,
+        std::size_t nx,
+        std::size_t ny,
+        double margin,
+        double triangleSize,
+        double waveHeight,
+        double offset) :
+    m_eulerAngles(eulerAngles),
+    m_friction(friction),
+    m_restitution(restitution),
+    m_size(size),
+    m_origin(origin),
+    m_nx(nx),
+    m_ny(ny),
+    m_margin(margin),
+    m_triangleSize(triangleSize),
+    m_waveHeight(waveHeight),
+    m_offset(offset)
+{
+    assert((m_friction >= 0.0) && (m_friction <= 1.0));
+    assert((m_restitution >= 0.0) && (m_restitution <= 1.0));
+    assert((m_size[0] >= 0.0) && (m_size[1] >= 0.0) && (m_size[2] >= 0.0));
+    assert(m_nx > 0);
+    assert(m_ny > 0);
+    assert(m_margin >= 0.0);
+    assert(m_triangleSize >= 0.0);
+    assert(m_waveHeight >= 0.0);
+    assert(m_offset >= 0.0);
+}
+
+tgImportGround::tgImportGround() :
+    m_config(Config())
+{
+    // @todo make constructor aux to avoid repeated code
+    pGroundShape = importCollisionShape();
+}
+
+tgImportGround::tgImportGround(const tgImportGround::Config& config) :
+    m_config(config)
+{
+    pGroundShape = importCollisionShape();
+}
+
+tgImportGround::~tgImportGround()
+{
+    delete m_pMesh;
+    delete[] m_pIndices;
+    delete[] m_vertices;
+}
+
+btRigidBody* tgImportGround::getGroundRigidBody() const
+{
+        std::cout << "Imported ground " << std::endl;
+    const btScalar mass = 0.0;
+
+    btTransform groundTransform;
+    groundTransform.setIdentity();
+    groundTransform.setOrigin(m_config.m_origin);
+
+    btQuaternion orientation;
+    orientation.setEuler(m_config.m_eulerAngles[0], // Yaw
+                         m_config.m_eulerAngles[1], // Pitch
+                         m_config.m_eulerAngles[2]); // Roll
+    groundTransform.setRotation(orientation);
+
+    // Using motionstate is recommended
+    // It provides interpolation capabilities, and only synchronizes 'active' objects
+    btDefaultMotionState* const pMotionState =
+        new btDefaultMotionState(groundTransform);
+
+    const btVector3 localInertia(0, 0, 0);
+
+    btRigidBody::btRigidBodyConstructionInfo const rbInfo(mass, pMotionState, pGroundShape, localInertia);
+
+    btRigidBody* const pGroundBody = new btRigidBody(rbInfo);
+
+    assert(pGroundBody);
+    return pGroundBody;
+}  
+
+btCollisionShape* tgImportGround::importCollisionShape() {
+    btCollisionShape * pShape = 0;
+    // The number of vertices in the mesh
+    // Hill Paramenters: Subject to Change
+    const std::size_t vertexCount = m_config.m_nx * m_config.m_ny;
+
+    if (vertexCount > 0) {
+        // The number of triangles in the mesh
+        const std::size_t triangleCount = 2 * (m_config.m_nx - 1) * (m_config.m_ny - 1);
+
+        // A flattened array of all vertices in the mesh
+        m_vertices = new btVector3[vertexCount];
+
+        // Supplied by the derived class
+        setVertices(m_vertices);
+        // A flattened array of indices for each corner of each triangle
+        m_pIndices = new int[triangleCount * 3];
+
+        // Supplied by the derived class
+        setIndices(m_pIndices);
+
+        // Create the mesh object
+        m_pMesh = createMesh(triangleCount, m_pIndices, vertexCount, m_vertices);
+
+        // Create the shape object
+        pShape = createShape(m_pMesh);
+
+        // Set the margin
+        pShape->setMargin(m_config.m_margin);
+        // DO NOT deallocate vertices, indices or pMesh until simulation is over!
+        // The shape owns them, but will not delete them
+    }
+
+    assert(pShape);
+    return pShape; 
+}
+
+btTriangleIndexVertexArray *tgImportGround::createMesh(std::size_t triangleCount, int indices[], std::size_t vertexCount, btVector3 vertices[]) {
+    const int vertexStride = sizeof(btVector3);
+    const int indexStride = 3 * sizeof(int);
+
+    btTriangleIndexVertexArray* const pMesh = 
+        new btTriangleIndexVertexArray(triangleCount,
+                indices,
+                indexStride,
+                vertexCount,
+                (btScalar*) &vertices[0].x(),
+                vertexStride);
+    return pMesh;
+}
+
+btCollisionShape *tgImportGround::createShape(btTriangleIndexVertexArray *pMesh) {
+    const bool useQuantizedAabbCompression = true;
+    btCollisionShape *const pShape = 
+        new btBvhTriangleMeshShape(pMesh, useQuantizedAabbCompression);
+    return pShape;
+}
+
+void tgImportGround::setVertices(btVector3 vertices[]) {
+    for (std::size_t i = 0; i < m_config.m_nx; i++)
+    {
+        for (std::size_t j = 0; j < m_config.m_ny; j++)
+        {
+            const btScalar x = (i - (m_config.m_nx * 0.5)) * m_config.m_triangleSize;
+            const btScalar y = (m_config.m_waveHeight * sin((double)i) * cos((double)j) +
+                    m_config.m_offset);
+            const btScalar z = (j - (m_config.m_ny * 0.5)) * m_config.m_triangleSize;
+            vertices[i + (j * m_config.m_nx)].setValue(x, y, z);
+        }
+    }
+}
+
+void tgImportGround::setIndices(int indices[]) {
+    int index = 0;
+    for (std::size_t i = 0; i < m_config.m_nx - 1; i++)
+    {
+        for (std::size_t j = 0; j < m_config.m_ny - 1; j++)
+        {
+            indices[index++] = (j       * m_config.m_nx) + i;
+            indices[index++] = (j       * m_config.m_nx) + i + 1;
+            indices[index++] = ((j + 1) * m_config.m_nx) + i + 1;
+
+            indices[index++] = (j       * m_config.m_nx) + i;
+            indices[index++] = ((j + 1) * m_config.m_nx) + i + 1;
+            indices[index++] = ((j + 1) * m_config.m_nx) + i;
+        }
+    }
+}
+
---
+++ src/core/terrain/tgImportGround.h
@@ -0,0 +1,155 @@
+/**
+ * Copyright © 2012, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ * 
+ * The NASA Tensegrity Robotics Toolkit (NTRT) v1 platform is licensed
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+#ifndef CORE_TERRAIN_TG_IMPORT_GROUND_H
+#define CORE_TERRAIN_TG_IMPORT_GROUND_H
+
+/**
+ * @file tgImportGround.h
+ * @brief Contains the definition of class tgImportGround.
+ * @author Edward Zhu
+ * $Id$
+ */
+
+#include "tgBulletGround.h"
+
+#include "LinearMath/btScalar.h"
+#include "LinearMath/btVector3.h"
+
+// std::size_t
+#include <cstddef>
+
+// Forward declarations
+class btRigidBody;
+class btTriangleIndexVertexArray;
+
+/**
+ * Ground imported from user generated .stl files
+ */
+class tgImportGround : public tgBulletGround
+{
+    public:
+
+        struct Config
+        {
+            public:
+                Config(btVector3 eulerAngles = btVector3(0.0, 0.0, 0.0),
+                       double friction = 0.5,
+                       double restitution = 0.0,
+                       btVector3 size = btVector3(500.0, 1.5, 500.0),
+                       btVector3 origin = btVector3(0.0, 0.0, 0.0),
+                       std::size_t nx = 50,
+                       std::size_t ny = 50,
+                       double margin = 0.05,
+                       double triangleSize = 5.0,
+                       double waveHeight = 5.0,
+                       double offset = 0.5);
+
+                /** Euler angles are specified as yaw pitch and roll */
+                btVector3 m_eulerAngles;
+
+                /** Friction value of the ground, must be between 0 to 1 */
+                btScalar  m_friction;
+
+                /** Restitution coefficient of the ground, must be between 0 to 1 */
+                btScalar  m_restitution;
+
+                /** Size of the ground, must be between non-negitive */
+                btVector3 m_size;
+
+                /** Origin position of the ground */
+                btVector3 m_origin;
+
+                /** Number of nodes in the x-direction */
+                std::size_t m_nx;
+
+                /** Number of nodes in the y-direction */
+                std::size_t m_ny;
+
+                /** See Bullet documentation on Collision Margin */
+                double m_margin;
+
+                /** Scale factor for the X and Z axes, varies according to m_nx and m_ny */
+                double m_triangleSize;
+
+                /** Scale factor for the Y axis */
+                double m_waveHeight;
+
+                /** Translation factor for the Y axis */
+                double m_offset;
+        };
+
+        /**
+         * Default construction that uses the default values of config
+         * Sets up a collision object that is stored in the bulletGround
+         * object
+         */
+        tgImportGround();
+
+        /**
+         * Allows a user to specify their own config
+         */
+        tgImportGround(const tgImportGround::Config& config);
+
+        /** Clean up the implementation. Deletes m_pMesh */
+        virtual ~tgImportGround();
+
+        /**
+         * Setup and return a return a rigid body based on the collision 
+         * object
+         */
+        virtual btRigidBody* getGroundRigidBody() const;
+
+        /**
+         * Returns the collision shape that forms the imported ground
+         */
+        btCollisionShape* importCollisionShape();
+
+    private:  
+        /** Store the configuration data for use later */
+        Config m_config;
+
+        /** Pre-condition: Quantity of triangles and vertices must each be greater than zero 
+         *  Post-condition: Returns a mesh, as configured by the input parameters, 
+         *                  to be used as a template for a btBvhTriangleMeshShape
+         */
+        btTriangleIndexVertexArray* createMesh(std::size_t triangleCount, int indices[], std::size_t vertexCount, btVector3 vertices[]);
+
+        /** Pre-condition: Given mesh is a valig btTriangleIndexVertexArray with all values initialized
+         *  Post-condition: Returns a btBvhTriangleMeshShape in the shape of the hills as configured 
+         */
+        btCollisionShape *createShape(btTriangleIndexVertexArray * pMesh);
+
+        /**
+         * @param[out] A flattened array of vertices in the mesh
+         */
+        void setVertices(btVector3 vertices[]);
+
+        /**
+         * @param[out] A flattened array of indices in the mesh
+         */
+        void setIndices(int indices[]);
+        
+        // Store this so we can delete it later
+        btTriangleIndexVertexArray* m_pMesh;
+        btVector3 * m_vertices;
+        int * m_pIndices;
+
+};
+
+#endif  // TG_IMPORT_GROUND_H
