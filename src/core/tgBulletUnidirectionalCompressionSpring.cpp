622ecfad4857b32ee8e564a66b93d0c0942f0531
Drew Sabelhaus <apsabelhaus@berkeley.edu>
2016-08-05 14:18:51 -0700

    First commit of the unidirectional compression spring. This commit does not compile.

---
+++ src/core/tgBulletUnidirectionalCompressionSpring.cpp
@@ -0,0 +1,269 @@
+/*
+ * Copyright © 2012, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ * 
+ * The NASA Tensegrity Robotics Toolkit (NTRT) v1 platform is licensed
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+*/
+
+/**
+ * @file tgBulletUnidirectionalCompressionSpring.cpp
+ * @brief Definitions of members of class tgBulletUnidirectionalCompressionSpring
+ * @author Drew Sabelhaus, Brian Mirletz, et al.
+ * @copyright Copyright (C) 2016 NASA Ames Research Center
+ * $Id$
+ */
+
+// This module
+#include "tgBulletUnidirectionalCompressionSpring.h"
+#include "tgBulletSpringCableAnchor.h"
+#include "tgCast.h"
+// The BulletPhysics library
+#include "BulletDynamics/Dynamics/btRigidBody.h"
+
+#include <iostream>
+#include <stdexcept>
+
+/**
+ * The main constructor for this class
+ * TO-DO as of 2016-08-04: this constructor needs to call the constructor
+ * for tgBulletCompressionSpring. Look up an example of how to do this...
+ */
+tgBulletUnidirectionalCompressionSpring::tgBulletUnidirectionalCompressionSpring(
+		const std::vector<tgBulletSpringCableAnchor*>& anchors,
+		bool isFreeEndAttached,
+                double coefK,
+                double coefD,
+                double restLength,
+		btVector3 direction) :
+m_dampingForce(0.0),
+m_velocity(0.0),
+m_isFreeEndAttached(isFreeEndAttached),
+m_coefK(coefK),
+m_coefD(coefD),
+m_restLength(restLength),
+m_direction(direction),
+m_anchors(anchors),
+anchor1(anchors.front()),
+anchor2(anchors.back())
+{
+    // There should be two anchors for a compression spring.
+    assert(m_anchors.size() == 2);
+
+    // Constructor from tgSpringCable:
+    assert(coefK > 0.0);
+    assert(coefD >= 0.0);
+
+    // the rest length of the spring has to be positive
+    if (m_restLength <= 0.0)
+    {
+	throw std::invalid_argument("Rest length for a compression spring must be postive.");
+    }
+	
+    m_prevLength = m_restLength;
+    
+    assert(invariant());
+}
+
+// Destructor has to the responsibility of destroying the anchors also.
+tgBulletUnidirectionalCompressionSpring::~tgBulletUnidirectionalCompressionSpring()
+{
+    #if (0)
+    std::cout << "Destroying tgBulletCompressionSpring" << std::endl;
+    #endif
+    
+    std::size_t n = m_anchors.size();
+    
+    // Make absolutely sure these are deleted, in case we have a poorly timed reset
+    if (m_anchors[0] != anchor1)
+    {
+        delete anchor1;
+    }
+    
+    if (m_anchors[n-1] != anchor2)
+    {
+        delete anchor2;
+    }
+    
+    for (std::size_t i = 0; i < n; i++)
+    {
+        delete m_anchors[i];
+    }
+
+    
+    m_anchors.clear();
+
+    // Delete the btVector3 also.
+    // TO-DO: does this need to happen? ...
+    //delete m_direction;
+}
+
+// The step function is what's called from other places in NTRT.
+void tgBulletUnidirectionalCompressionSpring::step(double dt)
+{
+    if (dt <= 0.0)
+    {
+        throw std::invalid_argument("dt is not positive!");
+    }
+
+    calculateAndApplyForce(dt);
+
+    // If the spring distance has gone negative, crash the simulator on purpose.
+    // TO-DO: find a way to apply a hard stop here instead.
+    if( getCurrentSpringLength() <= 0.0)
+    {
+      throw std::runtime_error("Compression spring has negative length, simulation stopping. Increase your stiffness coefficient.");
+    }
+    
+    assert(invariant());
+}
+
+/**
+ * Returns the current length of the spring. If isFreeEndAttached,
+ * this can be either greater or less than m_restLength. If not, then spring
+ * can only exist in compression (less than m_restLength).
+ * This only calculates the distance in the stated direction.
+ * TO-DO: start here on 2016-08-05.
+ */
+const double tgBulletUnidirectionalCompressionSpring::getCurrentSpringLength() const
+{
+    // initialize to the default value.
+    // if the distance between the two anchors is larger
+    // than the rest length of the spring, it means (intuitively) that
+    // one end of the spring is not touching a rigid body.
+    double springLength = getRestLength();
+
+    // store this variable so we don't have multiple calls to the same function.
+    double currAnchorDist = getCurrentAnchorDistance();
+
+    if( isFreeEndAttached() )
+    {
+      // The spring length is always the distance between anchors.
+      springLength = currAnchorDist;
+    }
+    else if( currAnchorDist < getRestLength() )
+    {
+      // the spring is not attached at its free end, but is in compression,
+      // so there is a length change.
+      springLength = currAnchorDist;
+    }
+    
+    return springLength;
+}
+
+/**
+ * Returns the current force in the spring.
+ * If ~isFreeEndAttached, 
+ * this is zero if the distance between anchors is larger than restLength.
+ * Note that this does NOT include the force due to the damper, since that force
+ * is only temporary and isn't reallyt "the force in the spring" per se.
+ */
+const double tgBulletUnidirectionalCompressionSpring::getSpringForce() const
+{
+    // Since the getCurrentSpringLength function already includes the check
+    // against m_isFreeEndAttached:
+    double springForce = - getCoefK() * (getCurrentSpringLength() - getRestLength());
+
+    // Debugging
+    #if (0)
+    std::cout << "getCoefK: " << getCoefK() << " getCurrentSpringLength(): "
+	      << getCurrentSpringLength() << " getRestLength: "
+	      << getRestLength() <<std::endl;
+    #endif
+    
+    // A negative delta_X should result in a positive force.
+    // note that if m_isFreeEndAttached == false, then springForce >= 0 always,
+    // since then the calculation above will have a (restlength - restLength) term.
+    // quick check to be sure of that:
+    if( !isFreeEndAttached())
+    {
+        assert(springForce >= 0.0);
+    }
+    
+    return springForce;
+}
+
+/**
+ * This is the method that does the heavy lifting in this class.
+ * Called by step, it calculates what force the spring is experiencing,
+ * and applies that force to the rigid bodies it connects to.
+ */
+void tgBulletUnidirectionalCompressionSpring::calculateAndApplyForce(double dt)
+{
+
+    // Create variables to hold the results of these computations
+    btVector3 force(0.0, 0.0, 0.0);
+    // the following ONLY includes forces due to K, not due to damping.
+    double magnitude = getSpringForce();
+
+    // hold this variable so we don't have to call the accessor function twice.
+    const double currLength = getCurrentSpringLength();
+
+    // Get the unit vector for the direction of the force, this is needed for
+    // applying the force to the rigid bodies.
+    const btVector3 dist =
+	  anchor2->getWorldPosition() - anchor1->getWorldPosition();
+    // The unit vector of the direction of the force will be needed later
+    // In order to have a positive force move the two rigid bodies away
+    // from each other, this unit vector must be in the opposite direction
+    // of this calculation. Otherwise, a positive force brings them closer
+    // together. Needs a minus.
+    // note that dist.length is a scalar double.
+    const btVector3 unitVector = - dist / currLength;
+
+    // Calculate the damping force for this timestep.
+    // Take an approximated derivative to estimate the velocity of the
+    // tip of the compression spring:
+    const double changeInDeltaX = currLength - m_prevLength;
+    m_velocity = changeInDeltaX / dt;
+
+    // The damping force for this timestep
+    // Like with spring force, a positive velocity should result in a
+    // force acting against the movement of the tip of the spring.
+    m_dampingForce = - getCoefD() * m_velocity;
+
+    // Debugging
+    #if (0)
+      std::cout << "Length: " << getCurrentSpringLength() << " rl: " << getRestLength() <<std::endl;
+      std::cout << "SpringForce: " << magnitude << " DampingForce: " << m_dampingForce <<std::endl;
+    #endif
+
+    // Add the damping force to the force from the spring.
+    magnitude += m_dampingForce;
+    
+    // Project the force into the direction of the line between the two anchors
+    force = unitVector * magnitude; 
+    
+    // Store the calculated length as the previous length
+    m_prevLength = currLength;
+    
+    //Now Apply it to the connected two bodies
+    btVector3 point1 = this->anchor1->getRelativePosition();
+    this->anchor1->attachedBody->activate();
+    this->anchor1->attachedBody->applyImpulse(force*dt,point1);
+    
+    btVector3 point2 = this->anchor2->getRelativePosition();
+    this->anchor2->attachedBody->activate();
+    this->anchor2->attachedBody->applyImpulse(-force*dt,point2);
+}
+
+bool tgBulletUnidirectionalCompressionSpring::invariant(void) const
+{
+    return (m_coefK > 0.0 &&
+    m_coefD >= 0.0 &&
+    m_prevLength >= 0.0 &&
+    m_restLength >= 0.0 &&
+    anchor1 != NULL &&
+    anchor2 != NULL &&
+    m_anchors.size() >= 2);
+}
---
+++ src/core/tgBulletUnidirectionalCompressionSpring.h
@@ -0,0 +1,197 @@
+/*
+ * Copyright © 2012, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ * 
+ * The NASA Tensegrity Robotics Toolkit (NTRT) v1 platform is licensed
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+*/
+
+#ifndef SRC_CORE_TG_BULLET_UNIDIRECTIONAL_COMPRESSION_SPRING_H_
+#define SRC_CORE_TG_BULLET_UNIDIRECTIONAL_COMPRESSION_SPRING_H_
+
+/**
+ * @file tgBulletUnidirectionalCompressionSpring.h
+ * @brief Definitions of class tgBulletUnidirectionalCompressionSpring
+ * @author Drew Sabelhaus, Brian Mirletz, Atil Iscen
+ * @copyright Copyright (C) 2016 NASA Ames Research Center
+ * $Id$
+ */
+
+// The Bullet Physics library
+#include "LinearMath/btVector3.h"
+// The C++ Standard Library
+#include <vector>
+
+// This class inherits from tgBulletCompressionSpring
+#include "tgBulletCompressionSpring.h"
+
+// Forward references
+// Note that even though this object is not a tgSpringCable,
+// we're still using tgSpringCableAnchor as a convenience.
+// This works because there actually don't seem to be any references
+// to tgSpringCable in tgSpringCableAnchor or tgBulletSpringCableAnchor.
+class btRigidBody;
+class tgSpringCableAnchor;
+class tgBulletSpringCableAnchor;
+
+/**
+ * This class defines the passive dynamics of a compression spring
+ * system, with damping, in the Bullet physics engine.
+ * It extends tgBulletCompressionSpring, and changes the way that forces
+ * are applied: instead of calculating and applying force along the vector
+ * between the two anchor points, it only calculates/applies force along
+ * one of the axes: X, Y or Z.
+ * So for example, if it's only in Z, one anchor could move around in X or Y
+ * and the applied force wouldn't change.
+ */
+class tgBulletUnidirectionalCompressionSpring : public tgBulletCompressionSpring
+{
+public: 
+    /**
+     * The only constructor. Takes a list of anchors, a flag controlling
+     * attached-ness of the free end of the spring, a coefficient
+     * of stiffness, a coefficent of damping, rest length
+     * of the spring, and a btVector3 in one of the X, Y, or Z directions.
+     * @param[in] anchors - a list of this spring cable's attachements
+     * @param[in] isFreeEndAttached - boolean flag.
+     * If no, the spring only provides compression force. 
+     * If yes, the spring is "attached" to both anchors, and provides a 
+     * tension force too when the distance between anchors is greater 
+     * than rest length.
+     * @param[in] coefK - the stiffness of the spring. Must be positive
+     * @param[in] dampingCoefficient - the damping in the spring. Must be non-negative.
+     * @param[in] restLength - the length of the compression spring when unloaded.
+     * @param[in] direction - the direction of the force to be applied, a btVector3.
+     * the current version will only support (1,0,0), (0,1,0), or (0,0,1).
+     */
+    tgBulletUnidirectionalCompressionSpring(
+		const std::vector<tgBulletSpringCableAnchor*>& anchors,
+	        bool isFreeEndAttached,
+                double coefK,
+                double coefD,
+                double restLength,
+		btVector3 direction);
+    
+    /**
+     * The virtual destructor. Deletes all of the anchors including 
+     * anchor1 and anchor2
+     */
+    virtual ~tgBulletUnidirectionalCompressionSpring();
+
+    /**
+     * Updates this object. Calls calculateAndApplyForce(dt)
+     * @param[in] dt, must be positive
+     */
+    virtual void step(double dt);
+
+    /**
+     * Returns either restLength or the length of the spring along the 
+     * single direction, depending on isFreeEndAttached.
+     */
+    virtual const double getCurrentSpringLength() const;
+    
+    /**
+     * Returns the force currently in the spring, either compression only / a positive force only (if isFreeEndAttached is false), or potentially either + or - force (if isFreeEndAttached == true).
+     */
+    virtual const double getSpringForce() const;
+
+    /**
+     * Return the direction of applied force for this spring
+     */
+    virtual const btVector3 getDirection() const
+    {
+        return m_direction;
+    }
+    
+protected:
+
+    /**
+     * Direction of the force that this spring will apply
+     */
+    btVector3 const m_direction;
+
+    /**
+     * The list of contact points. tgBulletSpringCable typically has two
+     * whereas tgBulletContactSpringCable will have more. 
+     * Needs to be stored here for consistent rendering.
+     * Vector has the convienence of tgCast functions, and we used to
+     * need a random iterator to sort
+     */
+   std::vector<tgBulletSpringCableAnchor*> m_anchors;
+    
+    /**
+     * The first attachement point for this spring cable. Storing it
+     * seperately makes a number of functions easier
+     */
+    tgBulletSpringCableAnchor * const anchor1;
+
+    /**
+     * The other permanent attachment for this spring cable. 
+     */
+    tgBulletSpringCableAnchor * const anchor2;
+
+    /**
+     * The force in the spring due to damping, at the last update step. 
+     * Stored so we can get it without passing a dt
+     */
+    double m_dampingForce;
+    
+    /**
+     * The velocity of the spring tip at the last update step. Stored so we
+     * can get it without passing a dt
+     */
+    double m_velocity;
+
+    /**
+     * Boolean flag controlling the application of either tension forces or not.
+     */
+    bool m_isFreeEndAttached;
+
+    /**
+     * The stiffness coefficient
+     * Units of mass / sec ^2
+     * Must be positive
+     */
+    const double m_coefK;
+
+    /**
+     * The damping coefficient.
+     * Units of mass / sec. 
+     * Must be non-negative
+     */
+    const double m_coefD;
+    
+    /**
+     * The rest length of the spring. Must be non negative
+     */
+    double m_restLength;
+ 
+    /**
+     * The previous actual length of the spring. Used when calculating
+     * force and velocity
+     */
+    double m_prevLength;
+    
+    /**
+     * Calculates the current forces that need to be applied to 
+     * the rigid bodies, and applies them to the bodies of anchor1 and 
+     * anchor2
+     */
+    virtual void calculateAndApplyForce(double dt);
+
+private: 
+    /** Ensures integrity of member variables */
+    bool invariant(void) const;
+};
+
+#endif  // SRC_CORE_TG_BULLET_UNIDIRECTIONAL_COMPRESSION_SPRING_H_
