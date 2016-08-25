61a3953ad5f702e17d496c69fbc2aa84c3a4bc1a
Drew Sabelhaus <apsabelhaus@berkeley.edu>
2016-08-25 14:06:03 -0400

    More work on tgBoxInfo detecting and adding nodes that are on its surface (right now, on or inside) instead of just the nodes at its from and to inside its pair.

--- src/tgcreator/tgBoxInfo.cpp
+++ src/tgcreator/tgBoxInfo.cpp
@@ -187,6 +187,138 @@ std::set<tgRigidInfo*> tgBoxInfo::getLeafRigids()
     return leaves;
 }
 
+/**
+ * This function is used in containsNode to check if a node is on the surface
+ * of the box that will be created by this tgBoxInfo.
+ * NOTE that is should only work right now for boxes that are orthogonal
+ * to the axes of the world: it "should" return false for all rotated boxes.
+ * This function is a close copy of Bullet Physics' btBoxShape::isInside.
+ */
+bool tgBoxInfo::isNodeOnBoxSurface(const btVector3& nodeVector) const {
+  // Like btBoxShape::isInside, first calculate the vector from the
+  // center of this box to the the node that's on the corner of the
+  // box where all coordinates are positive (the "top right" corner.)
+  // This vector starts from one node of the box, and goes halfway between
+  // the from and the to node (which should be the center of the box...)
+  btVector3 boxCenter = getFrom() + ( getTo() - getFrom() )/2;
+  // The "half extents" of the box are the x, y, and z dimensions.
+  btVector3 lengthContributionToHalfExtents;
+  btVector3 widthContributionToHalfExtents;
+  btVector3 heightContributionToHalfExtents;
+  // HOWEVER, we need to figure out which dimension corresponds to the length,
+  // width, and height of the box.
+  
+  // Length dimension is along the getFrom(), getTo() axis.
+  // Subtracting getTo() from getFrom() should result in zero along two axes...
+  lengthContributionToHalfExtents = (getTo() - getFrom()) / 2;
+  // DEBUGGING:
+  std::cout << "Inside isNodeOnBoxSurface: lengthContributionToHalfExtents == "
+	    << lengthContributionToHalfExtents << std::endl;
+  
+  // The coordinates go in a "counterclockwise" sort of direction,
+  // so for example if the z direction is length, then width is y and height is x,
+  // and if length is y, width is x and height is z.
+  if( lengthContributionToHalfExtents.z() != 0 ) {
+    //DEBUGGING
+    std::cout << "Length is in z direction, so width will be in y, "
+	      << "and height will be in x. " << std::endl;
+    widthContributionToHalfExtents = btVector3( 0, m_config.width/2, 0);
+    heightContributionToHalfExtents = btVector3( m_config.height/2, 0, 0);
+  }
+  else if( lengthContributionToHalfExtents.y() != 0 ) {
+    //DEBUGGING
+    std::cout << "Length is in y direction, so width will be in x, "
+	      << "and height will be in z. " << std::endl;
+    widthContributionToHalfExtents = btVector3( m_config.width/2, 0, 0);
+    heightContributionToHalfExtents = btVector3( 0, 0, m_config.height/2);
+  }
+  else if( lengthContributionToHalfExtents.x() != 0 ) {
+    //DEBUGGING
+    std::cout << "Length is in x direction, so width will be in z, "
+      	      << "and height will be in y. " << std::endl;
+    widthContributionToHalfExtents = btVector3( 0, 0, m_config.width/2);
+    heightContributionToHalfExtents = btVector3(0, m_config.height/2, 0);
+  }
+  else {
+    throw std::runtime_error("Length of box seems to be zero inside tgBoxInfo, something is very wrong.");
+  }
+  //DEBUGGING
+  std::cout << "Inside isNodeOnBoxSurface: widthContributionToHalfExtents == "
+	    << widthContributionToHalfExtents << std::endl;
+  std::cout << "Inside isNodeOnBoxSurface: heightContributionToHalfExtents == "
+	    << heightContributionToHalfExtents << std::endl;
+
+  // The half-extents are the sum of the contributions in each direction.
+  // Here, all the contributions should be in only one direction each!
+  btVector3 halfExtents = lengthContributionToHalfExtents
+    + widthContributionToHalfExtents
+    + heightContributionToHalfExtents;
+  
+  //DEBUGGING
+  if( 1 ){
+    std::cout << "Checking tgBoxInfo::isNodeOnBoxSurface for box with "
+	      << "pair (" << getFrom() << ", " << getTo() << "), and half-extents "
+	      << halfExtents <<", and node vector "
+	      << nodeVector << " ..." << std::endl;
+  }
+  // Point is on box surface if equal to either positive or negative extents
+  // in one dimension, and is within the +/- extents in the other two.
+  // This is checked by comparing the location of the point with respect to
+  // the box center plus or minus the half extents.
+  // For now, check inclusion on surface OR inside since that's easier.
+  bool result = (nodeVector.x() <= (boxCenter.x() + halfExtents.x())) &&
+    (nodeVector.x() >= (boxCenter.x() - halfExtents.x())) &&
+    (nodeVector.y() <= (boxCenter.y() + halfExtents.y())) &&
+    (nodeVector.y() >= (boxCenter.y() - halfExtents.y())) &&
+    (nodeVector.z() <= (boxCenter.z() + halfExtents.z())) &&
+    (nodeVector.z() >= (boxCenter.z() - halfExtents.z()));
+  //DEBUGGING
+  if( 1 ) {
+    std::cout << "Result of node surface check: " << result << std::endl;
+  }
+  return result;
+}
+
+/**
+ * This function has been enhanced to include checking of either endpoints
+ * or inclusion inside the box.
+ */
+bool tgBoxInfo::containsNode(const btVector3& nodeVector) const {
+  // DEBUGGING
+  if( 1 ){
+    std::cout << "Called containsNode inside tgBoxInfo with pair "
+	      << getFrom() << ", " << getTo() << " for nodeVector "
+	      << nodeVector << std::endl;
+  }
+  // The older version of this function:
+  //return ((getFrom() - nodeVector).fuzzyZero() || (getTo() - nodeVector).fuzzyZero());
+
+  // Instead, first do that check:
+  if( ((getFrom() - nodeVector).fuzzyZero() || (getTo() - nodeVector).fuzzyZero()) ){
+    //DEBUGGING:
+    std::cout << "containsNode: true." << std::endl;
+    return true;
+  }
+  else {
+    // Check if it's inside or on the surface of the box.
+    // This requies that m_collisionShape (which is a btBoxShape) to be set!
+    // So, check that first.
+    // NOTE that m_collisionShape is (almost?) always NULL at this point in the
+    // setup of the simulator, so let's create one temporarily
+    // for purposes of checking its isInside method.
+    //DEBUGGING:
+    std::cout << "Node not either of the coordinates in this box's pair. "
+	      << "Now checking inside or on surface of box... " << std::endl;
+    if( isNodeOnBoxSurface(nodeVector) ) {
+      //DEBUGGING
+      std::cout << "Node is on box surface, true! " << std::endl;
+    }
+
+    std::cout << "containsNode: false." << std::endl;
+    return false;
+  }
+}
+
 std::set<btVector3> tgBoxInfo::getContainedNodes() const {
     std::set<btVector3> contained;
     contained.insert(getFrom());
