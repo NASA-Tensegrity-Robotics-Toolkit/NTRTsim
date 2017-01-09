4308194dc5bf579a7e2f98c40768c4fda1b817b6
Drew Sabelhaus <apsabelhaus@berkeley.edu>
2017-01-09 13:16:48 -0800

    Reverted to master's SUPERball app.

--- src/examples/SUPERball/AppSUPERball.cpp
+++ src/examples/SUPERball/AppSUPERball.cpp
@@ -50,8 +50,8 @@ int main(int argc, char** argv)
     
     // Determine the angle of the ground in radians. All 0 is flat
     const double yaw = 0.0;
-    //const double pitch = M_PI/15.0;
-    const double pitch = 0.0;
+    const double pitch = M_PI/15.0;
+    //const double pitch = 0.0;
     const double roll = 0.0;
     const tgBoxGround::Config groundConfig(btVector3(yaw, pitch, roll));
     // the world will delete this
