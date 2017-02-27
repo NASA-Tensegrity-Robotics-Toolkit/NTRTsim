1f6771ec9391442dcdd6974654bcdabd3627daaa
Drew Sabelhaus <apsabelhaus@berkeley.edu>
2017-02-26 21:17:34 -0800

    Laika with rotating vertebra stands on the force plates, but tension needs to be adjusted quite a bit.

--- src/dev/laika/RotatingVertebra/AppRotatingVertebraSpine.cpp
+++ src/dev/laika/RotatingVertebra/AppRotatingVertebraSpine.cpp
@@ -129,7 +129,7 @@ int main(int argc, char** argv)
 
     // Create the controller for the rotating vertebra.
     double startTimeRot = 0.0;
-    btVector3 startTorqueRot = btVector3(0.0, 0, 0);
+    btVector3 startTorqueRot = btVector3(0, 0, 0);
     double phaseTwoTimeRot = 8.0;
     btVector3 phaseTwoTorqueRot = btVector3(0.0, 0, 0); // was 0.2
     std::string rodHingeTag = "rodForHinge";
