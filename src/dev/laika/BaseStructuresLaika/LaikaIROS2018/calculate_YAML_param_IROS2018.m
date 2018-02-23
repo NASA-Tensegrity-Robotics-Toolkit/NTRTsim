57e6232045e6a7c69d67e9130d571815b6accc8f
Andrew Sabelhaus <apsabelhaus@berkeley.edu>
2018-02-22 18:56:39 -0800

    Started building up the exact version of Laika for the IROS 2018 paper. Rigid bodies in place but not cables yet.

--- src/dev/laika/BaseStructuresLaika/LaikaIROS2018/calculate_YAML_param_IROS2018.m
+++ src/dev/laika/BaseStructuresLaika/LaikaIROS2018/calculate_YAML_param_IROS2018.m
@@ -10,10 +10,14 @@ clc;
 
 % (1) Density of rigid bodies (needs volume of each)
 % (2) Cable parameters (pretension, etc)
-% (3) Assembly positions (spine translations, etc
+% (3) Assembly positions (spine translations, etc)
 
 % I'm assuming that we've already done the math to create the robot's
 % leg, foot, shoulder, hip models.
+
+% All below, we use the following scaling.
+% For example, an s=100 means we're using length units of cm.
+s = 100;
 
 % Physical parameters of the models. Lengths in cm.
 % YAML uses half-extents for the box dimensions! These are*full* extents.
@@ -41,8 +45,8 @@ rod_r = 0.64; % cm. THis is roughly 1/4 inch.
 % We'll name them top, bottom, right, left (tbrl).
 v_t = [6.5, 0, 9.6];
 v_b = [6.5, 0, -9.6];
-v_r = [-6.5, 9.5, 0];
-v_l = [-6.5, -9.5, 0];
+v_r = [-6.5, 9.6, 0];
+v_l = [-6.5, -9.6, 0];
 % For the rotating vertebra: same thing.
 v_rot_t = [5.5, 0, 9.2];
 v_rot_b = [5.5, 0, -9.2];
@@ -66,7 +70,7 @@ rear_vol_cm = 2 * leg_vol + ...
            hip_T_height * hip_T_width * hip_T_length_Y;
 
 % Convert volume in cm^3 to m^3: that's (1e-2)^3.
-rear_vol_m = rear_vol_cm * 1e-6;
+rear_vol_m = rear_vol_cm * (1/ s^3);
        
 % Now, for density. We want the total force to be in N, for F = m*g.
 % Here, however, we're using g' = 100 * g, so total mass (in kg) should be
@@ -92,7 +96,7 @@ rear_mass_kg = 0.181; % was 181 grams
 % factor.) So for example, this would be 1 / (100^3) = 1e-6, again.
 
 rear_dens_si = rear_mass_kg / rear_vol_m;
-rear_dens_adj = rear_dens_si * 1e-6;
+rear_dens_adj = rear_dens_si * (1 / s^3);
 
 % Let's do the vertebrae. The length of each rod is just the distance
 % (vector norm).
@@ -106,18 +110,77 @@ rot_rod_l = norm(v_rot_r);
 v_vol_cm = 4 * ( pi * rod_r^2 * rod_l);
 v_rot_vol_cm = 4 * (pi * rod_r^2 * rot_rod_l);
 % converted to m^3:
-v_vol_m = v_vol_cm * 1e-6;
-v_rot_vol_m = v_rot_vol_cm * 1e-6;
+v_vol_m = v_vol_cm * (1/s^3);
+v_rot_vol_m = v_rot_vol_cm * (1/s^3);
 
 % one vertebra weighs:
 v_mass_kg = 0.145;
 
 % density is then:
 v_dens_si = v_mass_kg / v_vol_m;
-v_dens_adj = v_dens_si * 1e-6;
+v_dens_adj = v_dens_si * (1/s^3);
 v_rot_dens_si = v_mass_kg / v_rot_vol_m;
-v_rot_dens_adj = v_rot_dens_si * 1e-6;
+v_rot_dens_adj = v_rot_dens_si * (1/s^3);
 
+%% (2) Cable parameters.
+
+% We need to calculate the following, for each set of cables:
+% (a) pretension 
+% ...but also, spring constant, but we'll probably pull this out of a
+% spreadsheet on the google drive, so not calculated here.
+
+% Initial lengths of each set of cables. We'll split into 5 sets of
+% cables: one set for all the saddles, and one each for the 4 sets of
+% horizontal cables.
+
+% Holly set 3.12 inch for the bottom
+x0_bottom = 7.8;
+% Hunter's dimension was 3.75 inch = 9.5 cm
+x0_else = 9.5;
+
+% At equilibrium, with Laika standing, here were the "stretched" lengths
+% of each cable: (in cm, measured with calipers)
+x_saddle = 14.7;
+x_top = 10.1;
+x_sides = 9.9;
+x_bottom = 9.4;
+
+% Then, as we'll be doing F = k (x - x0), the total stretched length (x-x0)
+% for each will be:
+stretch_saddle = x_saddle - x0_else;
+stretch_top = x_top - x0_else;
+stretch_sides = x_sides - x0_else;
+stretch_bottom = x_bottom - x0_bottom;
+
+% From the spreadsheet, here are the spring constants for each segment.
+% Thicknesses are: 
+% Bottom: 0.35 inch
+% All else: (was it 1.6? see spreadsheet...)
+k_bottom = 291;
+k_else = 174;
+% in N/m.0.
+
+% Then, we can calculate "pretension", which is F = k * stretch
+% for each cable. E.g., we're placing Laika in its equilibrium
+% configuration at the start of the simulation.
+F_saddle = k_else * stretch_saddle;
+F_top = k_else * stretch_top;
+F_sides = k_else * stretch_sides;
+F_bottom = k_bottom * stretch_bottom;
+
+% And just to record it here, the offset between the vertebrae should
+% roughly be the same as the horizontal distance between two vertebrae
+% nodes, as in
+offset = x_sides;
+
+% According to our earlier dimensional analysis,
+% we've got to scale pretension force by the same quantity as gravity and
+% lengths.
+% Here, we're working in centimeters, so forces then become:
+F_saddle_adj = F_saddle * s;
+F_top_adj = F_top * s;
+F_sides_adj = F_sides * s;
+F_bottom_adj = F_bottom * s;
 
 
 
