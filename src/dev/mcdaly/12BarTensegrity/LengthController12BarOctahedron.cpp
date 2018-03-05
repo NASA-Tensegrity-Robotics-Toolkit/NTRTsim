64253b0776ceadd4dba5dc07a7c324c8a4003f89
hannah <hannah.petersson@hotmail.se>
2018-03-05 14:23:24 -0800

    Added new file

---
+++ src/dev/mcdaly/12BarTensegrity/LengthController12BarOctahedron.cpp
@@ -0,0 +1,201 @@
+/*
+ * Copyright © 2012, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ * 
+ * The NASA Tensegrity Robotics Toolkit (NTRT) v1 platform is licensed
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+*/
+
+/**
+ * @file LengthController12BarOctahedron.cpp
+ * @brief Implementation of LengthController12BarOctahedron.h
+ * @author Hannah Petersson, based on files from Drew Sabelhaus and Mallory Daly
+ * $Id$
+ */
+
+/* This length controller does... TBD
+ * 
+ * 
+ */
+
+// This module
+#include "LengthController12BarOctahedron.h"
+
+// This application
+#include "yamlbuilder/TensegrityModel.h"
+
+// This library
+#include "core/tgBasicActuator.h" // All controllers are based on this
+#include "core/tgSpringCableActuator.h" // OBS OBS is this for a system with springs?
+#include "core/tgString.h" // Combining strings with ints for naming structures
+#include "core/tgTags.h" // The ability to tag models and controllers
+#include "sensors/tgDataObserver.h" // For viewing the data
+
+// The C++ standard library 
+#include <cassert>
+#include <stdexcept>
+#include <vector>
+#include <iostream>
+#include "helpers/Filehelpers.h"
+#include <stdexcept>
+
+// Constructor assigns variables, does some simple sanity checks.
+// Also, initializes the accumulator variable timePassed so that it can
+// be incremented in onStep.
+LengthController12BarOctahedron::LengthController12BarOctahedron(double startTime, 
+							double minLength, 
+							double rate,
+							 std::vector<std::string> tagsToControl) :
+  m_startTime(startTime),
+  m_minLength(minLength),
+  m_rate,
+  m_tagsToControl(tagsToControl),
+  m_timePassed(0.0)
+{
+  // Start time must be greater than or equal to zero
+  if ( m_startTime < 0.0 ) {
+    throw std::invalid_argument("Start time must be greater than or equal to zero.");
+  }
+  // Min length must be between 0 and 1 (it is a percentage) 
+  else if( m_minLength > 1) {
+    throw std::invalid_argument("min_Length is a percent, must be less than 1. (100%)");
+  }
+  else if( m_minLength < 0.0 ) {
+    throw std::invalid_argument("min_Length is a percent, must be greater than 0.");
+  }
+  // Rate must be greater than zero
+  else if( rate < 0.0 ) {
+    throw std::invalid_argument("Rate cannot be negative.");
+  }
+  // Should there be checks made on the tags?
+}
+
+/**
+ * The initializeActuators method is called in onSetup to put pointers to 
+ * specific actuators in the cablesWithTags array, as well as store the initial
+ * rest lengths in the initialRL map.
+ */
+void LengthController12BarOctahedron::initializeActuators(TensegrityModel& subject, std::string tag) {
+
+  //DEBUGGING
+  std::cout << "Finding cables with the tag: " << tag << std::endl;
+  
+  // Pick out the actuators with the specified tag
+  std::vector<tgBasicActuator*> foundActuators = subject.find<tgBasicActuator>(tag);
+  std::cout << "The following cables were found and will be controlled: " << std::endl;
+
+  // Iterate through array and output strings to command line
+  for (std::size_t i = 0; i < foundActuators.size(); i++ ) {
+    std::cout << foundActuators[i] -> getTags() << std::endl;
+    // Add the rest length of the actuator at this time to the list of all initial rest lengths
+    initialRL[foundActuators[i]->getTags()] = foundActuators[i]->getRestLength();
+    // DEBUGGING
+    std::cout << "Cable rest length at t = 0 is: " << initialRL[foundActuators[i]->getTags()] << std::endl;
+}
+
+/**
+ * For this controller, the onSetup method initializes the actuators,
+ * which means just store pointers to them and record their rest lengths.
+ * This method calls the helper initializeActuators.
+ */
+void LengthController12BarOctahedron::onSetup(TensegrityModel& subject)
+{
+  std::cout << "Setting up the LengthController12BarOctahedron controller."  
+            << "Finding cables with tags: " << m_tagsToControl << std::endl;
+  cablesWithTags = {};
+ 
+  // For all the strings in the list, call initializeActuators.
+  std::vector<std::string>::iterator it;
+  for( it = m_tagsToControl.begin(); it < m_tagsToControl.end(); it++ ) {
+    // Call the helper for this tag
+    initializeActuators(subject, *it);
+  }   
+  
+  // Initialize flags 
+  m_retract = 1; // Begin in tetract mode 
+  m_finished = 0; // True when finished retracting and returning all cables 
+  
+  // Initialize cable index
+  m_cable_index = 0;
+  
+  // Output that controller setup is complete
+  std::cout << "Finished setting up the controller." << std::endl;
+}
+
+/**
+ * Changes the cables' lengths at some specified timestep.
+ * @param[in] subject - the TensegrityModel that is being controlled. Must
+ * have a list of allMuscles populated
+ * @param[in] dt, current timestep must be positive
+ */
+void LengthController12BarOctahedron::onStep(TensegrityModel& subject, double dt)
+{
+  // First, increment the accumulator variable
+  m_timePassed += dt;
+  // If it is past the time to start the controller, start
+  if( m_timePassed > m_startTime ) 
+  {
+    // Retract mode (retract each cable in sequence)
+    if( m_retract == 1 ) {
+      int i = m_cable_index; // Grab cable index. Unnecessary?
+      double currRestLength = cablesWithTags[i]->getRestLength(); // Grab current rest length
+      double minRestLength = initialRL[cablesWithTags[i]->getTags()] * m_minLength; // Calculate the minimum rest length for this cable
+      // If the current rest length is greater than the desired minimum
+      if( currRestLength > minRestLength ) {
+        // Output a progress bar for the controller, to track when controll occurs
+        std::cout << "Control occurs, cable index: " << i << std::endl;
+        // Adjust rest length of the actuator
+        double nextRestLength = currRestLength - m_rate*dt;
+        // DEBUGGING
+        stf::cout << "Next rest length: " << m_cable_index << std::endl;
+        cablesWithTags[i]->setControlInput(nextRestLength, dt);
+      }
+      else {
+         // Cable has been retracted to min length, go to next cable
+        m_cable_index ++;//? OBS OBS OBS
+        std::cout << "Cable index: " << m_cable_index << std::endl;
+        // If the cable index is equal to the number of cables, all cables have been retracted
+        // Move to return state
+        if( m_cable_index == cablesWithTags.size() ) {
+          m_cable_index = 0;
+          m_retract = 0;
+        }
+      }
+    }
+
+  // Return state
+  else if ( m_finished == 0 ) {
+    std::cout << "Make it to return state." << std::endl;
+    int i = m_cable_index; // Grab cable index 
+    double currRestLength = cablesWithTags[i]->getRestLength(); // Grab current rest length
+    double initialRestLength = initialRL[cablesWithTags[i]->getTags()]; // Calculate initial rest length for this cable
+    // If the current rest length is below initial rest length
+    if( currRestLength < initialRestLength ) {
+      // Output a progress bar for the controller to track when control occurs
+      std::cout << "Control occured. Cable index: " << i << std::endl;
+      double nextRestLength = currRestLength = m_rate*dt; // Adjust rest length of the actuator 
+      // DEBUGGING
+      std::cout << "Next rest length: " << nextRestLength << std::endl;
+      cablesWithTags[i]->setControlInput(nextRestLength, dt);
+    } 
+    else {
+      // Cable has been retracted to min length, go to next cable
+      m_cable_index ++; // OBS OBS OBS
+      std::cout << "Cable index: " << m_cable_index << std::endl;
+      // If the cable index is equal to the number of cables, all cables have been retracted. Move to return state. 
+      if( m_cable_index == cablesWithTags.size() ) { 
+        m_finished = 1;
+      }
+    } 
+  }
+}
---
+++ src/dev/mcdaly/12BarTensegrity/LengthController12BarOctahedron.h
@@ -0,0 +1,144 @@
+/*
+ * Copyright © 2012, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ * 
+ * The NASA Tensegrity Robotics Toolkit (NTRT) v1 platform is licensed
+ * under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+*/
+
+/**
+ * @file LengthController12BarOctahedron.h
+ * @brief Contains the definition of class LengthController12BarOctahedron
+ * @author Hannah Petersson, based on files from Drew Sabelhaus and Mallory Daly
+ * $Id$
+ */
+
+/* This length controller does... TBD
+ * 
+ * 
+ */
+
+#ifndef LENGTHCONTROLLER12BAROCTAHEDRON_H
+#define LENGTHCONTROLLER12BAROCTAHEDRON_H
+
+// The NTRT core library
+#include "core/tgObserver.h" // Basic controller libraries
+#include "core/tgSubject.h" //        -||-
+#include "core/tgTags.h" // The ability to tag models and controllers
+
+// The C++ standard library
+#include <string> 
+#include <vector>
+#include <map>
+
+// Forward declarations
+class TensegrityModel;
+class tgBasicActuator;
+
+/**
+ * A controller to apply the length change in the cables.
+ */
+class LengthController12BarOctahedron : public tgObserver<TensegrityModel>, public tgSubject<LengthControllerWithReturn>
+{
+public:
+
+  /**
+   * Construct a 12 bar tensegrity structure (octahedron) controller.
+   * @param[in] startTime, a double that determines when the controller
+   * begins its motion, how many seconds after the simulation starts.
+   * @param[in] minLength, a double that is the percent of the initial length
+   * that this controller will reduce down to. E.g., if minLength = 0.25, 
+   * controller will act until the rest length of the cables is 25% of initial.
+   * @param[in] rate, the rate at which the rest length of the cables will be
+   * changed. Expressed in meters/sec.
+   * @param[in] tagsToControl, a vector (array) of strings, which is a list of the 
+   * tags of all the
+   * cables upon which to act. All the cables which have a tag in this list of tags
+   * will be acted upon by this controller.
+   */
+   LengthController12BarOctahedron(double startTime, double minLength, double rate, std::vector<std::string> tagsToControl);
+ 
+   /**
+   * Nothing to delete, destructor must be virtual
+   */
+   virtual ~LengthController12BarOctahedron() { }
+
+  /**
+   * Apply the controller. On setup, adjust the cable
+   * lengths one time.
+   * @param[in] subject - the TensegrityModel that is being controlled. Must
+   * have a list of allMuscles populated // What is allMuscles?/h
+   */
+   virtual void onSetup(TensegrityModel& subject);
+
+  /**
+   * Changes the cables' lengths at some specified timestep.
+   * @param[in] subject - the TensegrityModel that is being controlled. Must
+   * have a list of allMuscles populated
+   * @param[in] dt, current timestep must be positive
+   */
+   virtual void onStep(TensegrityModel& subject, double dt);
+
+protected:
+
+  /**
+   * A helper function to find and initialize the actuators that this class
+   * will control.
+   * @param[in] tag, a string of the tag for which to search in the list of 
+   * actuators in this model.
+   */
+  void initializeActuators(TensegrityModel& subject, std::string tag);
+
+private: 
+  /**
+   * The private variables for each of the values passed in to the constructor.
+   */
+  double m_startTime;
+  double m_minLength;
+  double m_rate;
+  std::vector<std::string> m_tagsToControl;
+
+  /**
+   * Need an accumulator variable to determine when to start the controller.
+   */
+  double m_timePassed;
+
+  /**
+   * Need integer for current cable index
+   */
+  int m_cable_index;
+
+  /**
+   * A list of all the actuators to control. This is populated in onSetup
+   * by using m_tagsToControl.
+   */
+  std::vector<tgBasicActuator*> cablesWithTags;
+
+  /**
+   * The start length of each of the cables must be recorded.
+   * This map takes a string (the space-separated list of all the tags for
+   * an individual cable) and outputs a double (the rest length at time t=0.)
+   */
+  typedef std::map<tgTags, double> InitialRestLengths;
+  InitialRestLengths initialRL;
+
+  /** OBS OBS OBS OBS OBS
+   * Need a boolean for returning or retracting the cable, not clear which one to use at this point
+   */
+ // bool m_return;
+ // bool m_retract;
+ // bool m_finished; 
+
+};
+
+#endif // LENGTHCONTROLLER12BAROCTAHEDRON_H
