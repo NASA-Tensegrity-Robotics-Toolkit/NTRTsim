cec921ad49a9575d1f29e4531dd341d2c3647625
Andrew Sabelhaus <apsabelhaus@cmu.edu>
2020-10-11 20:44:26 -0400

    Single step gait complete.

--- src/dev/laika/belkaWalking/control_trajectories/generate_belka_traj.m
+++ src/dev/laika/belkaWalking/control_trajectories/generate_belka_traj.m
@@ -12,7 +12,7 @@ clc;
 dt = 0.01;
 % Specify the max time manually, for ease. 
 % We'll apply a hold on the last value for each input until this max time.
-t_max = 2;
+t_max = 10;
 
 % We'll specify pairs of [timepoint, value] that will be used to
 % interpolate.
@@ -28,28 +28,51 @@ t_max = 2;
 u1 = [0, 0;
     0.5, 0;
     0.6, 20;
-    0.8, 20];
+    2.8, 20;
+    2.9, 0];
 
 % Leg B
-u2 = [0, 0];
+u2 = [0, 0;
+    1.8, 0;
+    1.9, 20;
+    2.8, 20;
+    2.9, 0];
 
 % Leg C
-u3 = [0, 0];
+u3 = [0, 0;
+    1.0, 0;
+    1.1, 20;
+    2.8, 20;
+    2.9, 0];
 
 % Leg D
-u4 = [0, 0];
+u4 = [0, 0;
+    2.4, 0;
+    2.5, 20;
+    2.8, 20;
+    2.9, 0];
 
 % Spine Horizontal Left/Right, as a percent retraction.
 u5 = [0, 0;
     0.2, 0;
-    0.4, -0.08];
+    0.4, -0.08;
+    1.2, -0.08;
+    1.6, 0.18;
+    2.6, 0.18;
+    2.8, 0];
 
 % Spine Rotation CW/CCW, as a percent retraction.
 u6 = [0, 0;
     0.2, 0;
     0.4, -0.065;
-    1.0, -0.065;
-    1.2, 0.055];
+    0.8, -0.065;
+    1.0, 0.055;
+    1.2, 0.055;
+    1.3, 0.040;
+    2.0, 0.040;
+    2.2, -0.065;
+    2.6, -0.065;
+    2.8, 0];
 
 % To iterate nicer, later, put these all in a cell array
 u_pts = {u1, u2, u3, u4, u5, u6};
@@ -61,6 +84,8 @@ for i = 1:size(u_pts,2)
     % add in the last point
     u_pts{i}(end+1, :) = [t_max, hold_val];
 end
+
+% TO-DO: ADD A CHECK THAT TMAX IS GREATER THAN MAX OF THESE TIMES
 
 %% (2) Generate the interpolated timepoints for each.
 
@@ -78,9 +103,9 @@ for i = 1:size(u_pts,2)
     % We'll work in "pairs" of points for interpolated segments, so iterate
     % over the specified points. That's rows of u_i up until the last row
     for j = 1:size(u_i,1)-1
-        disp('i,j:');
-        i
-        j
+        %disp('i,j:');
+        %i
+        %j
         % Calculate the slope for this j-th segment for this i-th input.
         t0_ij = u_i(j,1);
         tf_ij = u_i(j+1, 1);
@@ -96,8 +121,8 @@ for i = 1:size(u_pts,2)
         % just a line. Easiest way is to do an offset to
         % the time indices, with intercept as previous value
         v_ij = m_ij * (t_ij - t0_ij) + v0_ij;
-        disp('vij:');
-        v_ij
+        %disp('vij:');
+        %v_ij
         % Insert into the appropriate place in the trajectory. 
         % ASSUMING THE TIMEPOINTS ARE IN INTERVALS OF DT, the index that
         % should start this segment is
